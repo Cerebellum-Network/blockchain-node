8b6ac905cc5d16bcf9c0eb590d0855c1ad6fe8d3
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-03-16 20:22:00 +0600

    An empty DAC Validator pallet crate

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use frame_support::pallet_prelude::*;
+pub use frame_system::pallet_prelude::*;
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {}
+
+	#[pallet::error]
+	pub enum Error<T> {}
+}
