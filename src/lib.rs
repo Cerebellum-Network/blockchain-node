6b322c656245dfcc313be53c204bfcd4587fcda6
Andrei Navoichyk <andrei@cere.io>
2023-04-11 17:18:47 +0200

    removed nomination pool (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -681,20 +681,6 @@ impl pallet_bags_list::Config for Runtime {
 	type Score = VoteWeight;
 }
 
-use sp_runtime::traits::Convert;
-pub struct BalanceToU256;
-impl Convert<Balance, sp_core::U256> for BalanceToU256 {
-	fn convert(balance: Balance) -> sp_core::U256 {
-		sp_core::U256::from(balance)
-	}
-}
-pub struct U256ToBalance;
-impl Convert<sp_core::U256, Balance> for U256ToBalance {
-	fn convert(n: sp_core::U256) -> Balance {
-		n.try_into().unwrap_or(Balance::max_value())
-	}
-}
-
 parameter_types! {
 	pub const LaunchPeriod: BlockNumber = 1 * 24 * 60 * MINUTES;
 	pub const VotingPeriod: BlockNumber = 1 * 24 * 60 * MINUTES;
