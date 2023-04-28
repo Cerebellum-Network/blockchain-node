4fa175b2e2f2973e47f489762b503b8377009114
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-04-28 19:50:11 +0600

    Print the number of broken sessions by CDN node

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,7 @@ pub use frame_system::{
 	offchain::{AppCrypto, CreateSignedTransaction, SendSignedTransaction, Signer, SigningTypes},
 	pallet_prelude::*,
 };
+pub use lite_json::json::JsonValue;
 pub use pallet::*;
 pub use pallet_ddc_staking::{self as ddc_staking};
 pub use pallet_session as session;
@@ -255,6 +256,34 @@ pub mod pallet {
 			let file_request = dac::fetch_file_request(&mock_data_url);
 			let bytes_sum = dac::get_proved_delivered_bytes_sum(&file_request);
 			log::info!("Proved bytes sum: {:?}", bytes_sum);
+
+			// Print the number of broken sessions per CDN node.
+			let aggregates_value = dac::fetch_aggregates(&data_provider_url, 77436).unwrap(); // 77436 is for a mock data
+			let aggregates_obj = aggregates_value.as_object().unwrap();
+			aggregates_obj
+				.into_iter()
+				.for_each(|(cdn_node_pubkey, cdn_node_aggregates_value)| {
+					// iterate over aggregates for each node
+					let cdn_node_aggregates_obj = cdn_node_aggregates_value.as_object().unwrap();
+					// Extract `nodeInterruptedSessions` field
+					let (_, cdn_node_interrupted_sessions_value) = cdn_node_aggregates_obj
+						.into_iter()
+						.find(|(key, _)| key.iter().copied().eq("nodeInterruptedSessions".chars()))
+						.unwrap();
+					let cdn_node_interrupted_sessions_obj =
+						cdn_node_interrupted_sessions_value.as_object().unwrap();
+					// Prepare CDN pubkey without heap allocated string
+					let cdn_node_pubkey_vecu8: Vec<u8> =
+						cdn_node_pubkey.iter().map(|c| *c as u8).collect();
+					let cdn_node_pubkey_str =
+						sp_std::str::from_utf8(&cdn_node_pubkey_vecu8).unwrap();
+					log::info!(
+						"Broken sessions per CDN node | Node {}: {} sessions broken",
+						cdn_node_pubkey_str,
+						cdn_node_interrupted_sessions_obj.len(), /* count sessions broken by the
+						                                          * node */
+					);
+				});
 
 			// Wait for signal.
 			let signal = Signal::<T>::get().unwrap_or(false);
