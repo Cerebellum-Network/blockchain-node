bf748db920f2be6107a47aef16e26493891354a4
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-05-05 18:34:18 +0600

    Base64 encoding func for validators' shared memory

--- src/shm.rs
+++ src/shm.rs
@@ -1,1 +1,12 @@
 //! Validators' "shared memory" module.
+
+use base64::prelude::*;
+use sp_std::prelude::*;
+
+/// Encodes a vector of bytes into a vector of characters using base64 encoding.
+pub fn base64_encode(input: &Vec<u8>) -> Vec<char> {
+	let mut buf = Vec::with_capacity(1024); // ToDo: calculate capacity
+	buf.resize(1024, 0);
+	BASE64_STANDARD.encode_slice(input, &mut buf).unwrap(); // ToDo: handle error
+	buf.iter().map(|&byte| byte as char).collect()
+}
