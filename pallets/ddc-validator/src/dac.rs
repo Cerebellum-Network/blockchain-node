ca9a68994ddf08fb51115a517db5638c94d37609
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-05-22 16:37:57 +0600

    Autoformat DAC Validator files

--- pallets/ddc-validator/src/dac.rs
+++ pallets/ddc-validator/src/dac.rs
@@ -395,11 +395,12 @@ pub(crate) fn fetch_data2(
 	era: EraIndex,
 ) -> Result<(String, Vec<BytesSent>, String, Vec<BytesReceived>), ()> {
 	let bytes_sent_query = get_bytes_sent_query_url(data_provider_url, era);
-	let bytes_sent_res: RedisFtAggregate = http_get_json(&bytes_sent_query).map_err(|_|())?;
+	let bytes_sent_res: RedisFtAggregate = http_get_json(&bytes_sent_query).map_err(|_| ())?;
 	let bytes_sent = BytesSent::get_all(bytes_sent_res);
 
 	let bytes_received_query = get_bytes_received_query_url(data_provider_url, era);
-	let bytes_received_res: RedisFtAggregate = http_get_json(&bytes_received_query).map_err(|_|())?;
+	let bytes_received_res: RedisFtAggregate =
+		http_get_json(&bytes_received_query).map_err(|_| ())?;
 	let bytes_received = BytesReceived::get_all(bytes_received_res);
 
 	Ok((bytes_sent_query, bytes_sent, bytes_received_query, bytes_received))
