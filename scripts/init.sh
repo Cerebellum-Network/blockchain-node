c6aefb0e42472433d16ca93476ab4a24be4cf6a0
Maksim Ramanenkau <cbnhrpo@gmail.com>
2023-05-31 18:14:23 +0200

    Update toolchain

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-rustup install nightly-2022-07-27
+rustup install nightly-2022-10-09
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2022-07-27
+rustup target add wasm32-unknown-unknown --toolchain nightly-2022-10-09
