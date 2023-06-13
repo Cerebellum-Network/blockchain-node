2044be799536a9845b453a71548e6fd4a3f93bda
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-06-13 14:18:26 +0600

    Remove unused import

--- pallets/ddc-staking/src/lib.rs
+++ pallets/ddc-staking/src/lib.rs
@@ -20,7 +20,7 @@ use frame_support::{
 };
 use scale_info::TypeInfo;
 use sp_runtime::{
-	traits::{AtLeast32BitUnsigned, CheckedSub, Saturating, Zero},
+	traits::{AtLeast32BitUnsigned, Saturating, Zero},
 	RuntimeDebug,
 };
 
