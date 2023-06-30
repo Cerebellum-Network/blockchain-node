e9a0df33b1ef005c54d63301461fdde173247d4a
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-06-30 14:29:05 +0600

    Add basic setup test

--- pallets/ddc-staking/src/tests.rs
+++ pallets/ddc-staking/src/tests.rs
@@ -35,3 +35,44 @@ fn set_settings_works() {
         assert_eq!(settings.storage_chill_delay, default_settings.storage_chill_delay);
     });
 }
+
+#[test]
+fn basic_setup_works() {
+    // Verifies initial conditions of mock
+	ExtBuilder::default().build_and_execute(|| {
+		// Account 11 is stashed and locked, and account 10 is the controller
+        assert_eq!(DdcStaking::bonded(&11), Some(10));
+		// Account 21 is stashed and locked, and account 20 is the controller
+        assert_eq!(DdcStaking::bonded(&21), Some(20));
+        // Account 1 is not a stashed
+        assert_eq!(DdcStaking::bonded(&1), None);
+
+		// Account 10 controls the stash from account 11, which is 100 units
+        assert_eq!(
+			DdcStaking::ledger(&10),
+			Some(StakingLedger {
+				stash: 11,
+				total: 100,
+				active: 100,
+                chilling: Default::default(),
+				unlocking: Default::default(),
+			})
+		);
+		// Account 20 controls the stash from account 21, which is 100 units
+		assert_eq!(
+			DdcStaking::ledger(&20),
+			Some(StakingLedger {
+				stash: 21,
+				total: 100,
+				active: 100,
+                chilling: Default::default(),
+				unlocking: Default::default(),
+			})
+		);
+        // Account 1 does not control any stash
+		assert_eq!(DdcStaking::ledger(&1), None);
+
+        // Cluster 1 settings are default
+        assert_eq!(DdcStaking::settings(1), Default::default());
+    });
+}
