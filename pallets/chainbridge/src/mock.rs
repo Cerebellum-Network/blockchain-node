b5d8e684414ac9ee70103c73ac6028784cb45efe
Maksim Ramanenkau <cbnhrpo@gmail.com>
2023-07-03 16:03:50 +0200

    Update substrate dependencies
    Update chainbridge
    
    Update chainbridge tests
    
    Update ddc-metrics-offchain-worker
    
    Update ddc-pallet
    
    Update erc20 pallet
    
    Update erc721 pallet
    
    Update ddc-staking pallet
    
    Update cere runtime
    
    Update cere-dev
    
    Try to update ./node/service
    
    Update CLI
    
    Update changelog
    
    Remove obsolete doc comments
    
    Add doc comment for `pallet-ddc-staking`
    
    Add `pallet-ddc-staking` genesis config
    
    Extend `cere-dev` genesis with DDC Staking
    
    Impl `pallet-ddc-staking` WeightInfo on unit
    
    Mention `GenesisConfig` in the crate doc comment
    
    Add mock runtime for tests
    
    Add cluster's staking settings setter tests
    
    Add basic setup test
    
    Add change controller test
    
    Autoformat `pallet-ddc-staking` files
    
    Test DDC staking as CDN participant
    
    Fix chill extrinsic benchmark
    
    Add `weights.rs` file handlebars template
    
    Regenerate weights for `pallet-ddc-staking`
    
    Update README.md in `pallet-ddc-staking`
    
    Bump `impl_version`
    
    Add dependencies for CI environment
    
    Update recent changes in ddc-staking
    
    Change the way of installin protoc
    
    Add dependencies for CI environment
    
    Update changelog format
    
    Update path for protoc installation
    
    Update path for protoc installation
    
    Rustfmt launcher script for the pre-commit hook
    
    Create pre-commit hook symlink by env setup script
    
    Autoformat with `cargo +nightly fmt`
    
    Rustfmt on save in VS Code
    
    Extend check workflow with rustfmt
    
    Ignore git hooks setup failure
    
    Fix imports
    
    Fix ddc-staking
    
    Run cargo fmt
    
    Fix tests
    
    Update changlelog layout

--- pallets/chainbridge/src/mock.rs
+++ pallets/chainbridge/src/mock.rs
@@ -3,34 +3,33 @@
 use super::*;
 
 use frame_support::{
-    assert_ok, ord_parameter_types, parameter_types, weights::Weight, PalletId,
-    traits::{Everything}
+	assert_ok, ord_parameter_types, parameter_types, traits::Everything, weights::Weight, PalletId,
 };
 use frame_system::{self as system};
 use sp_core::H256;
 use sp_runtime::{
-    testing::Header,
-    traits::{AccountIdConversion, BlakeTwo256, Block as BlockT, IdentityLookup},
-    Perbill,
+	testing::Header,
+	traits::{AccountIdConversion, BlakeTwo256, Block as BlockT, IdentityLookup},
+	Perbill,
 };
 
 use crate::{self as bridge, Config};
 pub use pallet_balances as balances;
 
 parameter_types! {
-    pub const BlockHashCount: u64 = 250;
-    pub const MaximumBlockWeight: Weight = Weight::from_ref_time(1024);
-    pub const MaximumBlockLength: u32 = 2 * 1024;
-    pub const AvailableBlockRatio: Perbill = Perbill::one();
-    pub const MaxLocks: u32 = 50;
+	pub const BlockHashCount: u64 = 250;
+	pub const MaximumBlockWeight: Weight = Weight::from_ref_time(1024);
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub const MaxLocks: u32 = 50;
 }
 
 impl frame_system::Config for Test {
     type BaseCallFilter = Everything;
     type BlockWeights = ();
     type BlockLength = ();
-    type Origin = Origin;
-    type Call = Call;
+    type RuntimeOrigin = RuntimeOrigin;
+    type RuntimeCall = RuntimeCall;
     type Index = u64;
     type BlockNumber = u64;
     type Hash = H256;
@@ -38,7 +37,7 @@ impl frame_system::Config for Test {
     type AccountId = u64;
     type Lookup = IdentityLookup<Self::AccountId>;
     type Header = Header;
-    type Event = Event;
+    type RuntimeEvent = RuntimeEvent;
     type BlockHashCount = BlockHashCount;
     type DbWeight = ();
     type Version = ();
@@ -55,17 +54,17 @@ impl frame_system::Config for Test {
 }
 
 parameter_types! {
-    pub const ExistentialDeposit: u64 = 1;
+	pub const ExistentialDeposit: u64 = 1;
 }
 
 ord_parameter_types! {
-    pub const One: u64 = 1;
+	pub const One: u64 = 1;
 }
 
 impl pallet_balances::Config for Test {
     type Balance = u64;
     type DustRemoval = ();
-    type Event = Event;
+    type RuntimeEvent = RuntimeEvent;
     type ExistentialDeposit = ExistentialDeposit;
     type AccountStore = System;
     type WeightInfo = ();
@@ -75,14 +74,14 @@ impl pallet_balances::Config for Test {
 }
 
 parameter_types! {
-    pub const TestChainId: u8 = 5;
-    pub const ProposalLifetime: u64 = 50;
+	pub const TestChainId: u8 = 5;
+	pub const ProposalLifetime: u64 = 50;
 }
 
 impl Config for Test {
-    type Event = Event;
+    type RuntimeEvent = RuntimeEvent;
     type AdminOrigin = frame_system::EnsureRoot<Self::AccountId>;
-    type Proposal = Call;
+    type Proposal = RuntimeCall;
     type ChainId = TestChainId;
     type ProposalLifetime = ProposalLifetime;
 }
@@ -91,15 +90,15 @@ type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
 frame_support::construct_runtime!(
-    pub enum Test where
-        Block = Block,
-        NodeBlock = Block,
-        UncheckedExtrinsic = UncheckedExtrinsic
-    {
-        System: system::{Pallet, Call, Event<T>},
-        Balances: balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-        Bridge: bridge::{Pallet, Call, Storage, Event<T>},
-    }
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic
+	{
+		System: system::{Pallet, Call, Event<T>},
+		Balances: balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		Bridge: bridge::{Pallet, Call, Storage, Event<T>},
+	}
 );
 
 // pub const BRIDGE_ID: u64 =
@@ -110,38 +109,34 @@ pub const ENDOWED_BALANCE: u64 = 100_000_000;
 pub const TEST_THRESHOLD: u32 = 2;
 
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    let bridge_id =  AccountIdConversion::into_account_truncating(&MODULE_ID);
-    let mut t = frame_system::GenesisConfig::default()
-        .build_storage::<Test>()
-        .unwrap();
-    pallet_balances::GenesisConfig::<Test> {
-        balances: vec![(bridge_id, ENDOWED_BALANCE)],
-    }
-    .assimilate_storage(&mut t)
-    .unwrap();
-    let mut ext = sp_io::TestExternalities::new(t);
-    ext.execute_with(|| System::set_block_number(1));
-    ext
+	let bridge_id = AccountIdConversion::into_account_truncating(&MODULE_ID);
+	let mut t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+	pallet_balances::GenesisConfig::<Test> { balances: vec![(bridge_id, ENDOWED_BALANCE)] }
+		.assimilate_storage(&mut t)
+		.unwrap();
+	let mut ext = sp_io::TestExternalities::new(t);
+	ext.execute_with(|| System::set_block_number(1));
+	ext
 }
 
 pub fn new_test_ext_initialized(
-    src_id: ChainId,
-    r_id: ResourceId,
-    resource: Vec<u8>,
+	src_id: ChainId,
+	r_id: ResourceId,
+	resource: Vec<u8>,
 ) -> sp_io::TestExternalities {
     let mut t = new_test_ext();
     t.execute_with(|| {
         // Set and check threshold
-        assert_ok!(Bridge::set_threshold(Origin::root(), TEST_THRESHOLD));
+        assert_ok!(Bridge::set_threshold(RuntimeOrigin::root(), TEST_THRESHOLD));
         assert_eq!(Bridge::relayer_threshold(), TEST_THRESHOLD);
         // Add relayers
-        assert_ok!(Bridge::add_relayer(Origin::root(), RELAYER_A));
-        assert_ok!(Bridge::add_relayer(Origin::root(), RELAYER_B));
-        assert_ok!(Bridge::add_relayer(Origin::root(), RELAYER_C));
+        assert_ok!(Bridge::add_relayer(RuntimeOrigin::root(), RELAYER_A));
+        assert_ok!(Bridge::add_relayer(RuntimeOrigin::root(), RELAYER_B));
+        assert_ok!(Bridge::add_relayer(RuntimeOrigin::root(), RELAYER_C));
         // Whitelist chain
-        assert_ok!(Bridge::whitelist_chain(Origin::root(), src_id));
+        assert_ok!(Bridge::whitelist_chain(RuntimeOrigin::root(), src_id));
         // Set and check resource ID mapped to some junk data
-        assert_ok!(Bridge::set_resource(Origin::root(), r_id, resource));
+        assert_ok!(Bridge::set_resource(RuntimeOrigin::root(), r_id, resource));
         assert_eq!(Bridge::resource_exists(r_id), true);
     });
     t
@@ -149,16 +144,16 @@ pub fn new_test_ext_initialized(
 
 // Checks events against the latest. A contiguous set of events must be provided. They must
 // include the most recent event, but do not have to include every past event.
-pub fn assert_events(mut expected: Vec<Event>) {
-    let mut actual: Vec<Event> = system::Module::<Test>::events()
+pub fn assert_events(mut expected: Vec<RuntimeEvent>) {
+    let mut actual: Vec<RuntimeEvent> = system::Module::<Test>::events()
         .iter()
         .map(|e| e.event.clone())
         .collect();
 
-    expected.reverse();
+	expected.reverse();
 
-    for evt in expected {
-        let next = actual.pop().expect("event expected");
-        assert_eq!(next, evt.into(), "Events don't match (actual,expected)");
-    }
+	for evt in expected {
+		let next = actual.pop().expect("event expected");
+		assert_eq!(next, evt.into(), "Events don't match (actual,expected)");
+	}
 }
