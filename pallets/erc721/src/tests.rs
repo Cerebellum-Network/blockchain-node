67e827b43b7f2c1d0af58a7b96894482da6c334d
Maksim Ramanenkau <cbnhrpo@gmail.com>
2023-07-05 17:38:11 +0200

    Update erc721 pallet

--- pallets/erc721/src/tests.rs
+++ pallets/erc721/src/tests.rs
@@ -1,6 +1,6 @@
 #![cfg(test)]
 
-use super::mock::{new_test_ext, Erc721, Origin, Test, USER_A, USER_B, USER_C};
+use super::mock::{new_test_ext, Erc721, RuntimeOrigin, Test, USER_A, USER_B, USER_C};
 use super::*;
 use frame_support::{assert_noop, assert_ok};
 use sp_core::U256;
@@ -14,7 +14,7 @@ fn mint_burn_tokens() {
         let metadata_b: Vec<u8> = vec![4, 5, 6];
 
         assert_ok!(Erc721::mint(
-            Origin::root(),
+            RuntimeOrigin::root(),
             USER_A,
             id_a,
             metadata_a.clone()
@@ -28,12 +28,12 @@ fn mint_burn_tokens() {
         );
         assert_eq!(Erc721::token_count(), 1.into());
         assert_noop!(
-            Erc721::mint(Origin::root(), USER_A, id_a, metadata_a.clone()),
+            Erc721::mint(RuntimeOrigin::root(), USER_A, id_a, metadata_a.clone()),
             Error::<Test>::TokenAlreadyExists
         );
 
         assert_ok!(Erc721::mint(
-            Origin::root(),
+            RuntimeOrigin::root(),
             USER_A,
             id_b,
             metadata_b.clone()
@@ -47,16 +47,16 @@ fn mint_burn_tokens() {
         );
         assert_eq!(Erc721::token_count(), 2.into());
         assert_noop!(
-            Erc721::mint(Origin::root(), USER_A, id_b, metadata_b.clone()),
+            Erc721::mint(RuntimeOrigin::root(), USER_A, id_b, metadata_b.clone()),
             Error::<Test>::TokenAlreadyExists
         );
 
-        assert_ok!(Erc721::burn(Origin::root(), id_a));
+        assert_ok!(Erc721::burn(RuntimeOrigin::root(), id_a));
         assert_eq!(Erc721::token_count(), 1.into());
         assert!(!<Tokens>::contains_key(&id_a));
         assert!(!<TokenOwner<Test>>::contains_key(&id_a));
 
-        assert_ok!(Erc721::burn(Origin::root(), id_b));
+        assert_ok!(Erc721::burn(RuntimeOrigin::root(), id_b));
         assert_eq!(Erc721::token_count(), 0.into());
         assert!(!<Tokens>::contains_key(&id_b));
         assert!(!<TokenOwner<Test>>::contains_key(&id_b));
@@ -72,28 +72,28 @@ fn transfer_tokens() {
         let metadata_b: Vec<u8> = vec![4, 5, 6];
 
         assert_ok!(Erc721::mint(
-            Origin::root(),
+            RuntimeOrigin::root(),
             USER_A,
             id_a,
             metadata_a.clone()
         ));
         assert_ok!(Erc721::mint(
-            Origin::root(),
+            RuntimeOrigin::root(),
             USER_A,
             id_b,
             metadata_b.clone()
         ));
 
-        assert_ok!(Erc721::transfer(Origin::signed(USER_A), USER_B, id_a));
+        assert_ok!(Erc721::transfer(RuntimeOrigin::signed(USER_A), USER_B, id_a));
         assert_eq!(Erc721::owner_of(id_a).unwrap(), USER_B);
 
-        assert_ok!(Erc721::transfer(Origin::signed(USER_A), USER_C, id_b));
+        assert_ok!(Erc721::transfer(RuntimeOrigin::signed(USER_A), USER_C, id_b));
         assert_eq!(Erc721::owner_of(id_b).unwrap(), USER_C);
 
-        assert_ok!(Erc721::transfer(Origin::signed(USER_B), USER_A, id_a));
+        assert_ok!(Erc721::transfer(RuntimeOrigin::signed(USER_B), USER_A, id_a));
         assert_eq!(Erc721::owner_of(id_a).unwrap(), USER_A);
 
-        assert_ok!(Erc721::transfer(Origin::signed(USER_C), USER_A, id_b));
+        assert_ok!(Erc721::transfer(RuntimeOrigin::signed(USER_C), USER_A, id_b));
         assert_eq!(Erc721::owner_of(id_b).unwrap(), USER_A);
     })
 }
