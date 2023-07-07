5cdaace5f081d91f89be7b6c3c02fa066a6b4a62
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-07-07 18:08:19 +0600

    Create pre-commit hook symlink by env setup script

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,3 +7,5 @@ echo "*** Initializing WASM build environment"
 rustup install nightly-2022-10-09
 
 rustup target add wasm32-unknown-unknown --toolchain nightly-2022-10-09
+
+ln -sf $PWD/scripts/pre-commit.sh $PWD/.git/hooks/pre-commit
