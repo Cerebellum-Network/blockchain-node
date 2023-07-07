ad7dd56d6075267993712ab74b3a78c484a23af1
Maksim Ramanenkau <cbnhrpo@gmail.com>
2023-07-07 11:22:55 +0200

    Update rpc

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -84,7 +84,6 @@ where
 		+ Send
 		+ 'static,
 	C::Api: substrate_frame_rpc_system::AccountNonceApi<Block, AccountId, Index>,
-	C::Api: pallet_contracts_rpc::ContractsRuntimeApi<Block, AccountId, Balance, BlockNumber, Hash>,
 	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance>,
 	C::Api: BabeApi<Block>,
 	C::Api: BlockBuilder<Block>,
@@ -93,7 +92,6 @@ where
 	B: sc_client_api::Backend<Block> + Send + Sync + 'static,
 	B::State: sc_client_api::backend::StateBackend<sp_runtime::traits::HashFor<Block>>,
 {
-	use pallet_contracts_rpc::{ContractsApiServer, Contracts};
 	use pallet_transaction_payment_rpc::{TransactionPaymentApiServer, TransactionPayment};
 	use sc_consensus_babe_rpc::{BabeApiServer, Babe};
 	use sc_finality_grandpa_rpc::GrandpaApiServer;
@@ -118,7 +116,6 @@ where
 	// Making synchronous calls in light client freezes the browser currently,
 	// more context: https://github.com/paritytech/substrate/pull/3480
 	// These RPCs should use an asynchronous caller instead.
-	io.merge(Contracts::new(client.clone()).into_rpc())?;
 	io.merge(TransactionPayment::new(client.clone()).into_rpc())?;
 	io.merge(
 		Babe::new(
