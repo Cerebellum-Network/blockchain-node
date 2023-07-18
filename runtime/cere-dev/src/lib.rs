eafc04b2741e5bce7d7d47049ab508beb448816f
Maksim Ramanenkau <cbnhrpo@gmail.com>
2023-07-18 16:18:58 +0200

    Fix contracts dissapearing

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -1444,6 +1444,7 @@ pub type Executive = frame_executive::Executive<
 			StakingMigrationV11OldPallet,
 		>,
 		pallet_staking::migrations::v12::MigrateToV12<Runtime>,
+		pallet_contracts::Migration<Runtime>,
 	),
 >;
 
