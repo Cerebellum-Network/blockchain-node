61e1ea35ef2cc85925de6e29d0d6193fddc77e7c
Maksim Ramanenkau <cbnhrpo@gmail.com>
2023-08-11 17:44:38 +0200

    Revert "Merge pull request #73 from Cerebellum-Network/hotfix/revert-4.8.0-dev"
    This reverts commit f4d81cfbc21b85a26e47dc560c30fbc32854bc15, reversing
    changes made to 4a7a1ac1412f5f6c758bf66466403aabcdef098e.

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -40,7 +40,7 @@ impl SubstrateCli for Cli {
 			#[cfg(feature = "cere-dev-native")]
 			"dev" => Box::new(cere_service::chain_spec::cere_dev_development_config()?),
 			#[cfg(feature = "cere-dev-native")]
-            "local" => Box::new(cere_service::chain_spec::cere_dev_local_testnet_config()?),
+			"local" => Box::new(cere_service::chain_spec::cere_dev_local_testnet_config()?),
 			path => {
 				let path = std::path::PathBuf::from(path);
 
@@ -181,6 +181,11 @@ pub fn run() -> sc_cli::Result<()> {
 					let (client, _, _, _) = cere_service::new_chain_ops(&config)?;
 					unwrap_client!(client, cmd.run(client.clone()))
 				}),
+				#[cfg(not(feature = "runtime-benchmarks"))]
+				BenchmarkCmd::Storage(_) =>
+					Err("Storage benchmarking can be enabled with `--features runtime-benchmarks`."
+						.into()),
+				#[cfg(feature = "runtime-benchmarks")]
 				BenchmarkCmd::Storage(cmd) => runner.sync_run(|config| {
 					let (client, backend, _, _) = cere_service::new_chain_ops(&config)?;
 					let db = backend.expose_db();
