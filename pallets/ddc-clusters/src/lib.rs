19ffa9ce5fa1d3ac050446e26c7eed711430e72a
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-10-17 12:18:18 +0600

    Allow `is_some_and` feature

--- pallets/ddc-clusters/src/lib.rs
+++ pallets/ddc-clusters/src/lib.rs
@@ -13,6 +13,7 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![recursion_limit = "256"]
+#![feature(is_some_and)] // ToDo: delete at rustc > 1.70
 
 use frame_support::pallet_prelude::*;
 use frame_system::pallet_prelude::*;
