45bfdf192b780e8a8e90ca2b98f5a64f6a1390e2
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-10-19 11:46:40 +0600

    New `ddc-primitives` crate with basic DDC entities

---
+++ primitives/src/lib.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+
+use sp_core::hash::H160;
+use sp_runtime::{AccountId32, RuntimeDebug};
+
+pub type ClusterId = H160;
+
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Clone, Encode, Decode, RuntimeDebug, TypeInfo, PartialEq)]
+pub enum NodePubKey {
+	StoragePubKey(StorageNodePubKey),
+	CDNPubKey(CDNNodePubKey),
+}
+
+pub type StorageNodePubKey = AccountId32;
+pub type CDNNodePubKey = AccountId32;
