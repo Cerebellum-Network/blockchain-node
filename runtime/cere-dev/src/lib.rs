48f11ee54faa5863df31e33ce244ae98b966e629
Maksim Ramanenkau <cbnhrpo@gmail.com>
2023-10-24 12:38:46 +0200

    Reduce by 2 orders of magnitude the constants changed in v4.7.1

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -726,7 +726,7 @@ parameter_types! {
 	pub const LaunchPeriod: BlockNumber = 1 * 24 * 60 * MINUTES;
 	pub const VotingPeriod: BlockNumber = 1 * 24 * 60 * MINUTES;
 	pub const FastTrackVotingPeriod: BlockNumber = 3 * 60 * MINUTES;
-	pub const MinimumDeposit: Balance = 5_000_000 * DOLLARS;
+	pub const MinimumDeposit: Balance = 50_000 * DOLLARS;
 	pub const EnactmentPeriod: BlockNumber = 1 * 24 * 60 * MINUTES;
 	pub const CooloffPeriod: BlockNumber = 7 * 24 * 60 * MINUTES;
 	pub const MaxProposals: u32 = 100;
@@ -802,7 +802,7 @@ impl pallet_collective::Config<CouncilCollective> for Runtime {
 }
 
 parameter_types! {
-	pub const CandidacyBond: Balance = 500_000_000 * DOLLARS;
+	pub const CandidacyBond: Balance = 5_000_000 * DOLLARS;
 	// 1 storage item created, key size is 32 bytes, value size is 16+16.
 	pub const VotingBondBase: Balance = deposit(1, 64);
 	pub const VotingBondFactor: Balance = 1 * DOLLARS;
@@ -876,12 +876,12 @@ impl pallet_membership::Config<pallet_membership::Instance1> for Runtime {
 
 parameter_types! {
 	pub const ProposalBond: Permill = Permill::from_percent(5);
-	pub const ProposalBondMinimum: Balance = 5_000_000 * DOLLARS;
+	pub const ProposalBondMinimum: Balance = 50_000 * DOLLARS;
 	pub const SpendPeriod: BlockNumber = 1 * DAYS;
 	pub const Burn: Permill = Permill::from_percent(0);
 	pub const TipCountdown: BlockNumber = 1 * DAYS;
 	pub const TipFindersFee: Percent = Percent::from_percent(20);
-	pub const TipReportDepositBase: Balance = 5_000_000 * DOLLARS;
+	pub const TipReportDepositBase: Balance = 50_000 * DOLLARS;
 	pub const DataDepositPerByte: Balance = 1 * DOLLARS;
 	pub const TreasuryPalletId: PalletId = PalletId(*b"py/trsry");
 	pub const MaximumReasonLength: u32 = 16384;
@@ -916,7 +916,7 @@ impl pallet_treasury::Config for Runtime {
 parameter_types! {
 	pub const BountyCuratorDeposit: Permill = Permill::from_percent(50);
 	pub const BountyValueMinimum: Balance = 10 * DOLLARS;
-	pub const BountyDepositBase: Balance = 5_000_000 * DOLLARS;
+	pub const BountyDepositBase: Balance = 50_000 * DOLLARS;
 	pub const CuratorDepositMultiplier: Permill = Permill::from_percent(50);
 	pub const CuratorDepositMin: Balance = 1 * DOLLARS;
 	pub const CuratorDepositMax: Balance = 100 * DOLLARS;
