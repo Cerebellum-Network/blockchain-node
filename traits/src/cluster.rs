f5abcdb0ad830c0c454251cc150bbe61ac955045
Raid Ateir <ateirraid@gmail.com>
2023-11-07 15:30:54 +0100

    substitute bond size check with cluster gov params

--- traits/src/cluster.rs
+++ traits/src/cluster.rs
@@ -5,8 +5,14 @@ pub trait ClusterVisitor<T: Config> {
 	fn cluster_has_node(cluster_id: &ClusterId, node_pub_key: &NodePubKey) -> bool;
 
 	fn ensure_cluster(cluster_id: &ClusterId) -> Result<(), ClusterVisitorError>;
+
+	fn get_bond_size(
+		cluster_id: &ClusterId,
+		node_pub_key: &NodePubKey,
+	) -> Result<u128, ClusterVisitorError>;
 }
 
 pub enum ClusterVisitorError {
 	ClusterDoesNotExist,
+	ClusterGovParamsNotSet,
 }
