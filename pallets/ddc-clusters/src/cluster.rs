4a939e1dce242037bda7b5c3b087db20a1ecae4b
yahortsaryk <egortsaryk@gmail.com>
2023-11-09 01:40:19 +0100

    feat: cluster missing parameters added

--- pallets/ddc-clusters/src/cluster.rs
+++ pallets/ddc-clusters/src/cluster.rs
@@ -1,11 +1,10 @@
 use crate::pallet::Error;
 use codec::{Decode, Encode};
 use ddc_primitives::ClusterId;
-use frame_support::{pallet_prelude::*, parameter_types, BoundedVec};
+use frame_support::{pallet_prelude::*, parameter_types};
 use scale_info::TypeInfo;
 use sp_runtime::Perbill;
 use sp_staking::EraIndex;
-use sp_std::vec::Vec;
 
 parameter_types! {
 	pub MaxClusterParamsLen: u16 = 2048;
@@ -15,21 +14,18 @@ parameter_types! {
 pub struct Cluster<AccountId> {
 	pub cluster_id: ClusterId,
 	pub manager_id: AccountId,
+	pub reserve_id: AccountId,
 	pub props: ClusterProps<AccountId>,
 }
 
 #[derive(Clone, Encode, Decode, RuntimeDebug, TypeInfo, PartialEq)]
 pub struct ClusterProps<AccountId> {
-	// this is a temporal way of storing cluster parameters as a stringified json,
-	// should be replaced with specific properties for cluster
-	pub params: BoundedVec<u8, MaxClusterParamsLen>,
 	pub node_provider_auth_contract: AccountId,
 }
 
 // ClusterParams includes Governance non-sensetive parameters only
 #[derive(Clone, Encode, Decode, RuntimeDebug, TypeInfo, PartialEq)]
 pub struct ClusterParams<AccountId> {
-	pub params: Vec<u8>,
 	pub node_provider_auth_contract: AccountId,
 }
 
@@ -57,16 +53,14 @@ impl<AccountId> Cluster<AccountId> {
 	pub fn new(
 		cluster_id: ClusterId,
 		manager_id: AccountId,
+		reserve_id: AccountId,
 		cluster_params: ClusterParams<AccountId>,
 	) -> Result<Cluster<AccountId>, ClusterError> {
 		Ok(Cluster {
 			cluster_id,
 			manager_id,
+			reserve_id,
 			props: ClusterProps {
-				params: match cluster_params.params.try_into() {
-					Ok(vec) => vec,
-					Err(_) => return Err(ClusterError::ClusterParamsExceedsLimit),
-				},
 				node_provider_auth_contract: cluster_params.node_provider_auth_contract,
 			},
 		})
@@ -77,10 +71,6 @@ impl<AccountId> Cluster<AccountId> {
 		cluster_params: ClusterParams<AccountId>,
 	) -> Result<(), ClusterError> {
 		self.props = ClusterProps {
-			params: match cluster_params.params.try_into() {
-				Ok(vec) => vec,
-				Err(_) => return Err(ClusterError::ClusterParamsExceedsLimit),
-			},
 			node_provider_auth_contract: cluster_params.node_provider_auth_contract,
 		};
 		Ok(())
