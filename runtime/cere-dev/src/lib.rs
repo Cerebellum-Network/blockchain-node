7ca5bf0553fba24f12bbe0b3b25a4211a29dd14b
Victor Genin <victor@fanai.io>
2023-11-13 14:41:15 +0200

    payout<->customers

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -1355,6 +1355,8 @@ impl pallet_ddc_payouts::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type PalletId = PayoutsPalletId;
 	type Currency = Balances;
+
+	type CustomerCharger = DdcCustomers;
 }
 
 construct_runtime!(
