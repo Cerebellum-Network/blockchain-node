b7ac195c3587c080f974c9205accc96f84ef9589
yahortsaryk <egortsaryk@gmail.com>
2023-12-08 14:59:34 +0100

    chore: PR #159 is backported

--- pallets/ddc-clusters/src/lib.rs
+++ pallets/ddc-clusters/src/lib.rs
@@ -15,6 +15,14 @@
 #![recursion_limit = "256"]
 #![feature(is_some_and)] // ToDo: delete at rustc > 1.70
 
+pub mod weights;
+use crate::weights::WeightInfo;
+
+#[cfg(feature = "runtime-benchmarks")]
+pub mod benchmarking;
+#[cfg(any(feature = "runtime-benchmarks", test))]
+pub mod testing_utils;
+
 #[cfg(test)]
 pub(crate) mod mock;
 #[cfg(test)]
@@ -30,7 +38,7 @@ use ddc_primitives::{
 };
 use ddc_traits::{
 	cluster::{ClusterCreator, ClusterVisitor, ClusterVisitorError},
-	staking::{StakingVisitor, StakingVisitorError},
+	staking::{StakerCreator, StakingVisitor, StakingVisitorError},
 };
 use frame_support::{
 	assert_ok,
@@ -66,7 +74,9 @@ pub mod pallet {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 		type NodeRepository: NodeRepository<Self>; // todo: get rid of tight coupling with nodes-pallet
 		type StakingVisitor: StakingVisitor<Self>;
+		type StakerCreator: StakerCreator<Self, BalanceOf<Self>>;
 		type Currency: LockableCurrency<Self::AccountId, Moment = Self::BlockNumber>;
+		type WeightInfo: WeightInfo;
 	}
 
 	#[pallet::event]
@@ -95,6 +105,8 @@ pub mod pallet {
 		/// Cluster candidate should not plan to chill.
 		NodeChillingIsProhibited,
 		NodeAuthContractCallFailed,
+		NodeAuthContractDeployFailed,
+		NodeAuthNodeAuthorizationNotSuccessful,
 	}
 
 	#[pallet::storage]
@@ -178,7 +190,7 @@ pub mod pallet {
 	where
 		T::AccountId: UncheckedFrom<T::Hash> + AsRef<[u8]>,
 	{
-		#[pallet::weight(10_000)]
+		#[pallet::weight(<T as pallet::Config>::WeightInfo::create_cluster())]
 		pub fn create_cluster(
 			origin: OriginFor<T>,
 			cluster_id: ClusterId,
@@ -197,7 +209,7 @@ pub mod pallet {
 			)
 		}
 
-		#[pallet::weight(10_000)]
+		#[pallet::weight(<T as pallet::Config>::WeightInfo::add_node())]
 		pub fn add_node(
 			origin: OriginFor<T>,
 			cluster_id: ClusterId,
@@ -246,7 +258,7 @@ pub mod pallet {
 			Ok(())
 		}
 
-		#[pallet::weight(10_000)]
+		#[pallet::weight(<T as pallet::Config>::WeightInfo::remove_node())]
 		pub fn remove_node(
 			origin: OriginFor<T>,
 			cluster_id: ClusterId,
@@ -267,7 +279,7 @@ pub mod pallet {
 		}
 
 		// Sets Governance non-sensetive parameters only
-		#[pallet::weight(10_000)]
+		#[pallet::weight(<T as pallet::Config>::WeightInfo::set_cluster_params())]
 		pub fn set_cluster_params(
 			origin: OriginFor<T>,
 			cluster_id: ClusterId,
@@ -285,7 +297,7 @@ pub mod pallet {
 		}
 
 		// Requires Governance approval
-		#[pallet::weight(10_000)]
+		#[pallet::weight(<T as pallet::Config>::WeightInfo::set_cluster_gov_params())]
 		pub fn set_cluster_gov_params(
 			origin: OriginFor<T>,
 			cluster_id: ClusterId,
@@ -499,6 +511,10 @@ pub mod pallet {
 			match error {
 				NodeProviderAuthContractError::ContractCallFailed =>
 					Error::<T>::NodeAuthContractCallFailed,
+				NodeProviderAuthContractError::ContractDeployFailed =>
+					Error::<T>::NodeAuthContractDeployFailed,
+				NodeProviderAuthContractError::NodeAuthorizationNotSuccessful =>
+					Error::<T>::NodeAuthNodeAuthorizationNotSuccessful,
 			}
 		}
 	}
