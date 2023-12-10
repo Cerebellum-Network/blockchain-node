9345f341648d784292d8bc8cddec99302c94682e
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-12-10 17:13:23 +0600

    Invoke dprint in pre-commit hook (#191)

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 # Prevent committing badly formatted code
+
 cargo fmt -- --check
 if [ $? -ne 0 ]; then
 	echo "Run \`cargo fmt\` to fix formatting issues before committing."
+	exit 1
+fi
+
+dprint check
+if [ $? -ne 0 ]; then
+	echo "Run \`dprint fmt\` to fix formatting issues before committing."
 	exit 1
 fi
