054685e244cb2f59ecd403899cb7898bbdebff9b
rakanalh <rakan.alhneiti@gmail.com>
2024-01-03 14:55:13 +0300

    Remove 0.9.33 migrations

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -1466,10 +1466,7 @@ pub type SignedPayload = generic::SignedPayload<RuntimeCall, SignedExtra>;
 /// Extrinsic type that has already been checked.
 pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, RuntimeCall, SignedExtra>;
 /// Runtime migrations
-type Migrations = (
-	pallet_election_provider_multi_phase::migrations::v1::MigrateToV1<Runtime>,
-	pallet_fast_unstake::migrations::v1::MigrateToV1<Runtime>,
-);
+type Migrations = ();
 
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
