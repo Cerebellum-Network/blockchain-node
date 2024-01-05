850f96b85cbbe2aac831b87df339a219d5fd1817
yahortsaryk <egortsaryk@gmail.com>
2024-01-05 20:55:04 +0100

    wip

--- traits/src/pallet.rs
+++ traits/src/pallet.rs
@@ -4,6 +4,10 @@ use frame_system::Config;
 pub type PalletsOriginOf<T> =
 	<<T as frame_system::Config>::RuntimeOrigin as OriginTrait>::PalletsOrigin;
 
+pub trait ConvertOrigin<Origin> {
+	fn convert_origin() -> Result<Origin, ()>;
+}
+
 pub trait PalletVisitor<T: Config> {
 	fn get_account_id() -> T::AccountId;
 }
