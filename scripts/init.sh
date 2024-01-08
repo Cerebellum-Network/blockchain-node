ef22c2b46fa3c7ccb76e8c0808bad87f82fb4c99
Rakan Al-Huneiti <rakan.alhneiti@gmail.com>
2024-01-08 14:33:34 +0300

    Minor fixes (#223)

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,9 +4,9 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-rustup install nightly-2022-10-09
+rustup install nightly-2023-10-22
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2022-10-09
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-10-22
 
 ln -sf $PWD/scripts/pre-commit.sh $PWD/.git/hooks/pre-commit || true
 ln -sf $PWD/scripts/pre-push.sh $PWD/.git/hooks/pre-push || true
