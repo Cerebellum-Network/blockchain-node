9980f9736de17e1b8a4fdfdfcb9e9295fe358dd2
rakanalh <rakan.alhneiti@gmail.com>
2024-01-15 19:12:28 +0300

    Set spec & transaction versions

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -130,10 +130,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 48700,
+	spec_version: 48800,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 12,
+	transaction_version: 13,
 	state_version: 0,
 };
 
