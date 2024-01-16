1e4c5b2cd4cf9fb4fc1c0ff3bde8050f4fc26089
rakanalh <rakan.alhneiti@gmail.com>
2024-01-16 15:22:10 +0300

    Actually, just remove contracts migrations

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -1477,7 +1477,6 @@ type Migrations = (
 	// this release they will be properly pruned after the bonding duration has
 	// elapsed)
 	pallet_grandpa::migrations::CleanupSetIdSessionMap<Runtime>,
-	pallet_contracts::Migration<Runtime>,
 );
 
 /// Executive: handles dispatch to the various modules.
