e9bc4be9a25dc92be601f558fcfeed6dc3a7240e
rakanalh <rakan.alhneiti@gmail.com>
2024-01-22 10:34:12 +0300

    Add preimage migration to v1

--- runtime/cere/src/lib.rs
+++ runtime/cere/src/lib.rs
@@ -1498,6 +1498,7 @@ type Migrations = (
 	// this release they will be properly pruned after the bonding duration has
 	// elapsed)
 	pallet_grandpa::migrations::CleanupSetIdSessionMap<Runtime>,
+	pallet_preimage::migration::v1::Migration<Runtime>,
 );
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
