54d106d2e7123af3b1ed6b796d2ff6a8e605bca4
Rakan Al-Huneiti <rakan.alhneiti@gmail.com>
2024-01-24 01:54:36 +0400

    Upgrade to substrate 0.9.40 (#246)
    ### Description
    <!-- Describe what change this PR is implementing -->
    
    ### Types of Changes
    <!--- What types of changes does your code introduce? -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [x] Dependency upgrade (A change in substrate or any 3rd party crate
    version)
    
    ### Migrations and Hooks
    <!--- Check the following box with an x if the following applies: -->
    - [x] This change requires a runtime migration.
    - [ ] Modifies `on_initialize`
    - [ ] Modifies `on_finalize`
    
    ### Checklist
    <!--- All boxes need to be checked. Follow this checklist before
    requiring PR review -->
    - [x] Change has been tested locally.
    - [ ] Change adds / updates tests.
    - [x] Changelog doc updated.

--- pallets/ddc-payouts/src/lib.rs
+++ pallets/ddc-payouts/src/lib.rs
@@ -115,8 +115,12 @@ pub mod pallet {
 
 	use super::*;
 
+	/// The current storage version.
+	const STORAGE_VERSION: frame_support::traits::StorageVersion =
+		frame_support::traits::StorageVersion::new(0);
+
 	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
+	#[pallet::storage_version(STORAGE_VERSION)]
 	#[pallet::without_storage_info]
 	pub struct Pallet<T>(_);
 
