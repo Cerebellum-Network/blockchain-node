a64b91a0f4953de1aa1ce277a233a732b96ae932
aie0 <149175774+aie0@users.noreply.github.com>
2024-01-25 14:55:52 +0200

    Payout batch events (#248)
    ### Description
    <!-- Describe what change this PR is implementing -->
    
    ### Types of Changes
    <!--- What types of changes does your code introduce? -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Dependency upgrade (A change in substrate or any 3rd party crate
    version)
    
    ### Migrations and Hooks
    <!--- Check the following box with an x if the following applies: -->
    - [ ] This change requires a runtime migration.
    - [ ] Modifies `on_initialize`
    - [ ] Modifies `on_finalize`
    
    ### Checklist
    <!--- All boxes need to be checked. Follow this checklist before
    requiring PR review -->
    - [x] Change has been tested locally.
    - [x] Change adds / updates tests.
    - [x] Changelog doc updated.

--- pallets/ddc-payouts/src/lib.rs
+++ pallets/ddc-payouts/src/lib.rs
@@ -201,6 +201,7 @@ pub mod pallet {
 		Rewarded {
 			cluster_id: ClusterId,
 			era: DdcEra,
+			batch_index: BatchIndex,
 			node_provider_id: T::AccountId,
 			rewarded: u128,
 			expected_to_reward: u128,
@@ -208,6 +209,7 @@ pub mod pallet {
 		NotDistributedReward {
 			cluster_id: ClusterId,
 			era: DdcEra,
+			batch_index: BatchIndex,
 			node_provider_id: T::AccountId,
 			expected_reward: u128,
 			distributed_reward: BalanceOf<T>,
@@ -734,11 +736,13 @@ pub mod pallet {
 						&updated_billing_report.vault,
 					) - <T as pallet::Config>::Currency::minimum_balance();
 
+					// 10000000000001 > 10000000000000 but is still ok
 					if reward > vault_balance {
 						if reward - vault_balance > max_dust {
 							Self::deposit_event(Event::<T>::NotDistributedReward {
 								cluster_id,
 								era,
+								batch_index,
 								node_provider_id: node_provider_id.clone(),
 								expected_reward: amount_to_reward,
 								distributed_reward: vault_balance,
@@ -766,6 +770,7 @@ pub mod pallet {
 				Self::deposit_event(Event::<T>::Rewarded {
 					cluster_id,
 					era,
+					batch_index,
 					node_provider_id,
 					rewarded: reward_,
 					expected_to_reward: amount_to_reward,
