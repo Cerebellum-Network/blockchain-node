ec1a72df58c9f42db3435443fe23cf897e74dc58
yahortsaryk <egortsaryk@gmail.com>
2024-01-29 15:36:12 +0100

    fix: max nodes count in a cluster is reduced and the voting threshold is fixed

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -16,7 +16,7 @@ pub type ClusterId = H160;
 pub type DdcEra = u32;
 pub type BucketId = u64;
 pub type StorageNodePubKey = AccountId32;
-pub type ClusterNodesCount = u32;
+pub type ClusterNodesCount = u16;
 pub const MIN_VALIDATED_NODES_COUNT: ClusterNodesCount = 3;
 
 // ClusterParams includes Governance non-sensetive parameters only
