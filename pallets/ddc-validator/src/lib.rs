ee44b34abb1e3de8064782da90ee7873a633c8f6
Victor Genin <victor.genin@live.com>
2024-02-20 10:50:21 +0200

    staking trait

--- pallets/ddc-validator/src/lib.rs
+++ pallets/ddc-validator/src/lib.rs
@@ -22,9 +22,13 @@ mod mock;
 mod tests;
 
 pub use alloc::{format, string::String};
+pub use core::fmt::Debug;
+
 pub use alt_serde::{de::DeserializeOwned, Deserialize, Serialize};
 pub use codec::{Decode, Encode, HasCompact, MaxEncodedLen};
-pub use core::fmt::Debug;
+use ddc_primitives::traits::cluster::ClusterVisitor as ClusterVisitorType;
+use ddc_primitives::traits::staking::StakingVisitor as StakingVisitorType;
+use frame_election_provider_support::SortedListProvider;
 pub use frame_support::{
 	decl_event, decl_module, decl_storage, defensive,
 	dispatch::DispatchResult,
@@ -40,27 +44,13 @@ pub use frame_system::{
 	pallet_prelude::*,
 };
 pub use pallet::*;
-pub use pallet_session as session;
-pub use pallet_staking::{self as staking};
 pub use scale_info::TypeInfo;
-pub use sp_core::crypto::{AccountId32, KeyTypeId, UncheckedFrom};
-pub use sp_io::{crypto::sr25519_public_keys, offchain_index};
 pub use sp_runtime::offchain::{
 	http, storage::StorageValueRef, storage_lock, storage_lock::StorageLock, Duration, Timestamp,
 };
 pub use sp_staking::EraIndex;
 pub use sp_std::{collections::btree_map::BTreeMap, prelude::*};
-use ddc_primitives::{
-	traits::{
-		cluster::{ClusterVisitor as ClusterVisitorType}
-	}
-};
 extern crate alloc;
-
-/// The balance type of this pallet.
-type BalanceOf<T> = <<T as pallet_contracts::Config>::Currency as Currency<
-	<T as frame_system::Config>::AccountId,
->>::Balance;
 
 /// Offchain local storage key that holds the last era in which the validator completed its
 /// assignment.
@@ -72,8 +62,13 @@ const VALIDATION_LOCK: &[u8; 37] = b"pallet-ddc-validator::validation_lock";
 /// DDC validation, set it to false (0x00) or delete the key to disable it.
 const ENABLE_DDC_VALIDATION_KEY: &[u8; 21] = b"enable-ddc-validation";
 
+pub type BalanceOf<T> =
+<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
 #[frame_support::pallet]
 pub mod pallet {
+	use frame_election_provider_support::SortedListProvider;
+	use frame_support::traits::LockableCurrency;
 	use super::*;
 
 	#[pallet::pallet]
@@ -86,6 +81,8 @@ pub mod pallet {
 		/// The overarching event type.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
+		type Currency: LockableCurrency<Self::AccountId, Moment = Self::BlockNumber>;
+
 		/// Something that provides randomness in the runtime. Required by the tasks assignment
 		/// procedure.
 		type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
@@ -93,9 +90,9 @@ pub mod pallet {
 		/// A dispatchable call.
 		type RuntimeCall: From<Call<Self>>;
 
-		type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
-
 		type ClusterVisitor: ClusterVisitorType<Self>;
+		type ValidatorsList: SortedListProvider<Self::AccountId>;
+		type StakingVisitor: StakingVisitorType<Self, BalanceOf<Self>>;
 	}
 
 	/// The map from the era and validator stash key to the list of CDN nodes to validate.
@@ -108,12 +105,6 @@ pub mod pallet {
 	#[pallet::storage]
 	#[pallet::getter(fn signal)]
 	pub(super) type Signal<T: Config> = StorageValue<_, bool>;
-
-	/// The map from the era and CDN participant stash key to the validation decision related.
-	#[pallet::storage]
-	#[pallet::getter(fn validation_decisions)]
-	pub type ValidationDecisions<T: Config> =
-		StorageDoubleMap<_, Twox64Concat, EraIndex, Identity, T::AccountId, ValidationDecision>;
 
 	// Map to check if validation decision was performed for the era
 	#[pallet::storage]
@@ -158,22 +149,15 @@ pub mod pallet {
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config>
-	where
-		<T as frame_system::Config>::AccountId: AsRef<[u8]> + UncheckedFrom<T::Hash>,
-		<BalanceOf<T> as HasCompact>::Type: Clone + Eq + PartialEq + Debug + TypeInfo + Encode,
-	{
+	pub enum Event<T: Config> {
 		// Validator submits decision for an era
-		ValidationDecision(EraIndex, T::AccountId, ValidationDecision),
+		ValidationDecision(EraIndex, T::AccountId),
 		// Set era reward points
 		EraRewardPoints(EraIndex, Vec<(T::AccountId, u64)>),
 	}
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T>
-	where
-		<T as frame_system::Config>::AccountId: AsRef<[u8]> + UncheckedFrom<T::Hash>,
-		<BalanceOf<T> as HasCompact>::Type: Clone + Eq + PartialEq + Debug + TypeInfo + Encode,
 	{
 		fn on_initialize(block_number: T::BlockNumber) -> Weight {
 			if block_number <= 1u32.into() {
@@ -182,7 +166,7 @@ pub mod pallet {
 
 			Signal::<T>::set(Some(false));
 
-			let current_ddc_era = match ddc_staking::pallet::Pallet::<T>::current_era() {
+			let current_ddc_era = match T::StakingVisitor::current_era() {
 				Some(era) => era,
 				None => {
 					log::debug!("DDC era not set.");
@@ -233,7 +217,7 @@ pub mod pallet {
 				_ => 0, // let's consider an absent or undecodable data as we never did a validation
 			};
 
-			let current_ddc_era = match ddc_staking::pallet::Pallet::<T>::current_era() {
+			let current_ddc_era = match T::StakingVisitor::current_era() {
 				Some(era) => era,
 				None => {
 					defensive!("DDC era not set");
@@ -253,209 +237,202 @@ pub mod pallet {
 				return
 			}
 
-			if let Err(e) = Self::validate_edges() {
+			/*if let Err(e) = Self::validate_edges() {
 				log::warn!("🔎 DDC validation failed. {}", e);
 				return
-			}
+			} */
+
 			last_validated_era_storage.set(&current_ddc_era);
 			log::info!("🔎 DDC validation complete for {} era.", current_ddc_era);
 		}
 	}
 
 	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/*
+		/// Set validation decision for a given CDN node in an era.
+		///
+		/// Only registered validator keys can call this extrinsic.
+		/// Validation decision can be set only once per era per CDN node.
+		/// CDN node should be active.
+		///
+		#[pallet::weight(10_000)]
+		pub fn set_validation_decision(
+			origin: OriginFor<T>,
+			era: EraIndex,
+			cdn_node: T::AccountId,
+			validation_decision: ValidationDecision,
+		) -> DispatchResult {
+			let ddc_valitor_key = ensure_signed(origin)?;
+
+			ensure!(
+				DDCValidatorToStashKeys::<T>::contains_key(&ddc_valitor_key),
+				Error::<T>::DDCValidatorKeyNotRegistered
+			);
+
+			ensure!(
+				staking::Validators::<T>::contains_key(
+					Self::get_stash_for_ddc_validator(&ddc_valitor_key).unwrap()
+				),
+				Error::<T>::NotValidatorStash
+			);
+
+			ensure!(
+				!Self::validation_decision_set_for_node(era, &cdn_node),
+				Error::<T>::ValidationDecisionAlreadySet
+			);
+
+			ensure!(
+				<ddc_staking::pallet::Edges<T>>::contains_key(&cdn_node),
+				Error::<T>::NodeNotActive
+			);
+
+			ValidationDecisions::<T>::insert(era, cdn_node.clone(), validation_decision.clone());
+
+			ValidationDecisionSetForNode::<T>::insert(era, cdn_node.clone(), true);
+
+			Self::deposit_event(Event::<T>::ValidationDecision(era, cdn_node, validation_decision));
+
+			Ok(())
+		}
+
+		/// Set reward points for CDN participants at the given era.
+		///
+		/// Only registered validator keys can call this exstrinsic.
+		/// Reward points can be set only once per era per CDN node.
+		///	CDN node should be active.
+		///
+		/// `stakers_points` is a vector of (stash account ID, reward points) pairs. The rewards
+		/// distribution will be based on total reward points, with each CDN participant receiving a
+		/// proportionate reward based on their individual reward points.
+		#[pallet::weight(100_000)]
+		pub fn set_era_reward_points(
+			origin: OriginFor<T>,
+			era: EraIndex,
+			stakers_points: Vec<(T::AccountId, u64)>,
+		) -> DispatchResult {
+			let ddc_valitor_key = ensure_signed(origin)?;
+
+			ensure!(
+				DDCValidatorToStashKeys::<T>::contains_key(&ddc_valitor_key),
+				Error::<T>::DDCValidatorKeyNotRegistered
+			);
+
+			ensure!(
+				staking::Validators::<T>::contains_key(
+					Self::get_stash_for_ddc_validator(&ddc_valitor_key).unwrap()
+				),
+				Error::<T>::NotValidatorStash
+			);
+
+			let mut rewards_counter = 0;
+
+			<ddc_staking::pallet::ErasEdgesRewardPoints<T>>::mutate(era, |era_rewards| {
+				for (staker, points) in stakers_points.clone().into_iter() {
+					if !Self::reward_points_set_for_node(era, &staker) {
+						// ToDo deal with edge case when node is chilling
+						if <ddc_staking::pallet::Edges<T>>::contains_key(&staker) {
+							*era_rewards.individual.entry(staker.clone()).or_default() += points;
+							era_rewards.total += points;
+							<ddc_staking::pallet::ErasEdgesRewardPointsPerNode<T>>::mutate(
+								&staker,
+								|current_reward_points| {
+									let rewards: ddc_staking::EraRewardPointsPerNode =
+										ddc_staking::EraRewardPointsPerNode { era, points };
+									current_reward_points.push(rewards);
+								},
+							);
+							RewardPointsSetForNode::<T>::insert(era, staker, true);
+							rewards_counter += 1;
+						}
+					}
+				}
+			});
+
+			if rewards_counter > 0 {
+				Self::deposit_event(Event::<T>::EraRewardPoints(era, stakers_points));
+			}
+
+			Ok(())
+		} */
+
+		/// Extrinsic registers a ddc validator key for future use
+		///
+		/// Only controller of validator can call this extrinsic
+		/// Validator has to be in the active set
+		#[pallet::weight(10_000)]
+		pub fn set_validator_key(
+			origin: OriginFor<T>,
+			ddc_validator_pub: T::AccountId,
+		) -> DispatchResult {
+			let controller = ensure_signed(origin)?;
+			let ledger = T::StakingVisitor::active_stake(&controller).ok_or(Error::<T>::NotController)?;
+
+			ensure!(
+				T::ValidatorsList::contains(&ledger.stash),
+				Error::<T>::NotValidatorStash
+			);
+
+			DDCValidatorToStashKeys::<T>::insert(ddc_validator_pub, &ledger.stash);
+			Ok(())
+		}
+	}
+
 	impl<T: Config> Pallet<T>
-	where
-		<T as frame_system::Config>::AccountId: AsRef<[u8]> + UncheckedFrom<T::Hash>,
-		<BalanceOf<T> as HasCompact>::Type: Clone + Eq + PartialEq + Debug + TypeInfo + Encode,
 	{
-    /*
-    /// Set validation decision for a given CDN node in an era.
-    ///
-    /// Only registered validator keys can call this extrinsic.
-    /// Validation decision can be set only once per era per CDN node.
-    /// CDN node should be active.
-    ///
-    #[pallet::weight(10_000)]
-    pub fn set_validation_decision(
-        origin: OriginFor<T>,
-        era: EraIndex,
-        cdn_node: T::AccountId,
-        validation_decision: ValidationDecision,
-    ) -> DispatchResult {
-        let ddc_valitor_key = ensure_signed(origin)?;
+		fn assign(quorum_size: usize, era: EraIndex) -> Result<(), DispatchError> {
+			/*
+			let validators: Vec<T::AccountId> = DDCValidatorToStashKeys::<T>::iter_keys().collect();
+			log::debug!("Current validators: {:?}.", validators);
 
-        ensure!(
-            DDCValidatorToStashKeys::<T>::contains_key(&ddc_valitor_key),
-            Error::<T>::DDCValidatorKeyNotRegistered
-        );
+			if validators.len() == 0 {
+				return Err(AssignmentError::NoValidators)
+			}
 
-        ensure!(
-            staking::Validators::<T>::contains_key(
-                Self::get_stash_for_ddc_validator(&ddc_valitor_key).unwrap()
-            ),
-            Error::<T>::NotValidatorStash
-        );
+			if validators.len() < quorum_size {
+				return Err(AssignmentError::NotEnoughValidators {
+					requested_quorum: quorum_size,
+					available_validators: validators.len(),
+				})
+			}
 
-        ensure!(
-            !Self::validation_decision_set_for_node(era, &cdn_node),
-            Error::<T>::ValidationDecisionAlreadySet
-        );
+			let edges: Vec<T::AccountId> = <ddc_staking::pallet::Edges<T>>::iter_keys().collect();
+			log::debug!("Current edges: {:?}.", edges);
 
-        ensure!(
-            <ddc_staking::pallet::Edges<T>>::contains_key(&cdn_node),
-            Error::<T>::NodeNotActive
-        );
+			if edges.len() == 0 {
+				return Ok(())
+			}
 
-        ValidationDecisions::<T>::insert(era, cdn_node.clone(), validation_decision.clone());
+			let shuffled_validators = Self::shuffle(validators);
+			let shuffled_edges = Self::shuffle(edges);
 
-        ValidationDecisionSetForNode::<T>::insert(era, cdn_node.clone(), true);
+			let validators_keys: Vec<String> = shuffled_validators
+				.iter()
+				.map(|v| utils::account_to_string::<T>(v.clone()))
+				.collect();
 
-        Self::deposit_event(Event::<T>::ValidationDecision(era, cdn_node, validation_decision));
+			// Create several groups of validators `quorum_size` length each.
+			let quorums = Self::split(validators_keys, quorum_size);
 
-        Ok(())
-    }
+			// Write an assignment to each validator in each quorum. The difference between the
+			// number of edges assigned to each validator is not higher then 1. If the number of
+			// edges is less then the number of quorums, some quorums will not have any edges
+			// assigned.
+			let mut quorums_cycle = quorums.iter().cycle();
+			for edge in shuffled_edges {
+				let Some(quorum_validators) = quorums_cycle.next() else {
+					return Err(AssignmentError::DefensiveEmptyQuorumsCycle);
+				};
+				quorum_validators.iter().for_each(|validator| {
+					Assignments::<T>::append(
+						era,
+						utils::string_to_account::<T>(validator.clone()),
+						edge.clone(),
+					);
+				});
+			} */
 
-    /// Set reward points for CDN participants at the given era.
-    ///
-    /// Only registered validator keys can call this exstrinsic.
-    /// Reward points can be set only once per era per CDN node.
-    ///	CDN node should be active.
-    ///
-    /// `stakers_points` is a vector of (stash account ID, reward points) pairs. The rewards
-    /// distribution will be based on total reward points, with each CDN participant receiving a
-    /// proportionate reward based on their individual reward points.
-    #[pallet::weight(100_000)]
-    pub fn set_era_reward_points(
-        origin: OriginFor<T>,
-        era: EraIndex,
-        stakers_points: Vec<(T::AccountId, u64)>,
-    ) -> DispatchResult {
-        let ddc_valitor_key = ensure_signed(origin)?;
-
-        ensure!(
-            DDCValidatorToStashKeys::<T>::contains_key(&ddc_valitor_key),
-            Error::<T>::DDCValidatorKeyNotRegistered
-        );
-
-        ensure!(
-            staking::Validators::<T>::contains_key(
-                Self::get_stash_for_ddc_validator(&ddc_valitor_key).unwrap()
-            ),
-            Error::<T>::NotValidatorStash
-        );
-
-        let mut rewards_counter = 0;
-
-        <ddc_staking::pallet::ErasEdgesRewardPoints<T>>::mutate(era, |era_rewards| {
-            for (staker, points) in stakers_points.clone().into_iter() {
-                if !Self::reward_points_set_for_node(era, &staker) {
-                    // ToDo deal with edge case when node is chilling
-                    if <ddc_staking::pallet::Edges<T>>::contains_key(&staker) {
-                        *era_rewards.individual.entry(staker.clone()).or_default() += points;
-                        era_rewards.total += points;
-                        <ddc_staking::pallet::ErasEdgesRewardPointsPerNode<T>>::mutate(
-                            &staker,
-                            |current_reward_points| {
-                                let rewards: ddc_staking::EraRewardPointsPerNode =
-                                    ddc_staking::EraRewardPointsPerNode { era, points };
-                                current_reward_points.push(rewards);
-                            },
-                        );
-                        RewardPointsSetForNode::<T>::insert(era, staker, true);
-                        rewards_counter += 1;
-                    }
-                }
-            }
-        });
-
-        if rewards_counter > 0 {
-            Self::deposit_event(Event::<T>::EraRewardPoints(era, stakers_points));
-        }
-
-        Ok(())
-    } */
-
-    /// Extrinsic registers a ddc validator key for future use
-    ///
-    /// Only controller of validator can call this extrinsic
-    /// Validator has to be in the active set
-    #[pallet::weight(10_000)]
-    pub fn set_validator_key(
-        origin: OriginFor<T>,
-        ddc_validator_pub: T::AccountId,
-    ) -> DispatchResult {
-        let controller = ensure_signed(origin)?;
-        let ledger = staking::Ledger::<T>::get(&controller).ok_or(Error::<T>::NotController)?;
-
-        ensure!(
-            staking::Validators::<T>::contains_key(&ledger.stash),
-            Error::<T>::NotValidatorStash
-        );
-
-        DDCValidatorToStashKeys::<T>::insert(ddc_validator_pub, &ledger.stash);
-        Ok(())
-    }
-}
-
-impl<T: Config> Pallet<T>
-where
-    <T as frame_system::Config>::AccountId: AsRef<[u8]> + UncheckedFrom<T::Hash>,
-    <BalanceOf<T> as HasCompact>::Type: Clone + Eq + PartialEq + Debug + TypeInfo + Encode,
-{
-    fn assign(quorum_size: usize, era: EraIndex) -> Result<(), DispatchError> {
-    /*
-        let validators: Vec<T::AccountId> = DDCValidatorToStashKeys::<T>::iter_keys().collect();
-        log::debug!("Current validators: {:?}.", validators);
-
-        if validators.len() == 0 {
-            return Err(AssignmentError::NoValidators)
-        }
-
-        if validators.len() < quorum_size {
-            return Err(AssignmentError::NotEnoughValidators {
-                requested_quorum: quorum_size,
-                available_validators: validators.len(),
-            })
-        }
-
-        let edges: Vec<T::AccountId> = <ddc_staking::pallet::Edges<T>>::iter_keys().collect();
-        log::debug!("Current edges: {:?}.", edges);
-
-        if edges.len() == 0 {
-            return Ok(())
-        }
-
-        let shuffled_validators = Self::shuffle(validators);
-        let shuffled_edges = Self::shuffle(edges);
-
-        let validators_keys: Vec<String> = shuffled_validators
-            .iter()
-            .map(|v| utils::account_to_string::<T>(v.clone()))
-            .collect();
-
-        // Create several groups of validators `quorum_size` length each.
-        let quorums = Self::split(validators_keys, quorum_size);
-
-        // Write an assignment to each validator in each quorum. The difference between the
-        // number of edges assigned to each validator is not higher then 1. If the number of
-        // edges is less then the number of quorums, some quorums will not have any edges
-        // assigned.
-        let mut quorums_cycle = quorums.iter().cycle();
-        for edge in shuffled_edges {
-            let Some(quorum_validators) = quorums_cycle.next() else {
-                return Err(AssignmentError::DefensiveEmptyQuorumsCycle);
-            };
-            quorum_validators.iter().for_each(|validator| {
-                Assignments::<T>::append(
-                    era,
-                    utils::string_to_account::<T>(validator.clone()),
-                    edge.clone(),
-                );
-            });
-        } */
-
-        Ok(())
-    }
-
+			Ok(())
+		}
 	}
 }
