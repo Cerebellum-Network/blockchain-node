e2bdc1cfe0c50b8d81039e99a68a59d3eee9e280
rakanalh <rakan.alhneiti@gmail.com>
2024-02-20 23:31:30 +0300

    Remove redundant clones

--- pallets/ddc-payouts/src/tests.rs
+++ pallets/ddc-payouts/src/tests.rs
@@ -1196,7 +1196,7 @@ fn send_charging_customers_batch_works_for_day_free_storage() {
 		assert_eq!(System::events().len(), 5 + 3 + 1); // 1 for Currency::transfer
 
 		// batch 2
-		let mut before_total_customer_charge = report.total_customer_charge.clone();
+		let mut before_total_customer_charge = report.total_customer_charge;
 		batch_index += 1;
 		assert_ok!(DdcPayouts::send_charging_customers_batch(
 			RuntimeOrigin::signed(dac_account),
@@ -1439,7 +1439,7 @@ fn send_charging_customers_batch_works_for_day_free_stream() {
 		assert_eq!(System::events().len(), 5 + 3 + 1); // 1 for Currency::transfer
 
 		// batch 2
-		let mut before_total_customer_charge = report.total_customer_charge.clone();
+		let mut before_total_customer_charge = report.total_customer_charge;
 		batch_index += 1;
 		assert_ok!(DdcPayouts::send_charging_customers_batch(
 			RuntimeOrigin::signed(dac_account),
@@ -1682,7 +1682,7 @@ fn send_charging_customers_batch_works_for_day_free_get() {
 		assert_eq!(System::events().len(), 5 + 3 + 1); // 1 for Currency::transfer
 
 		// batch 2
-		let mut before_total_customer_charge = report.total_customer_charge.clone();
+		let mut before_total_customer_charge = report.total_customer_charge;
 		batch_index += 1;
 		assert_ok!(DdcPayouts::send_charging_customers_batch(
 			RuntimeOrigin::signed(dac_account),
@@ -1925,7 +1925,7 @@ fn send_charging_customers_batch_works_for_day_free_put() {
 		assert_eq!(System::events().len(), 5 + 3 + 1); // 1 for Currency::transfer
 
 		// batch 2
-		let mut before_total_customer_charge = report.total_customer_charge.clone();
+		let mut before_total_customer_charge = report.total_customer_charge;
 		batch_index += 1;
 		assert_ok!(DdcPayouts::send_charging_customers_batch(
 			RuntimeOrigin::signed(dac_account),
@@ -2168,7 +2168,7 @@ fn send_charging_customers_batch_works_for_day_free_storage_stream() {
 		assert_eq!(System::events().len(), 5 + 3 + 1); // 1 for Currency::transfer
 
 		// batch 2
-		let mut before_total_customer_charge = report.total_customer_charge.clone();
+		let mut before_total_customer_charge = report.total_customer_charge;
 		batch_index += 1;
 		assert_ok!(DdcPayouts::send_charging_customers_batch(
 			RuntimeOrigin::signed(dac_account),
