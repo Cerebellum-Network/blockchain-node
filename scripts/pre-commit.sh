a2e16e235e9c5dd1cad509c05ac4ad5990e9c211
Maksim Ramanenkau <cbnhrpo@gmail.com>
2024-03-04 13:09:33 +0100

    Set short timings

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,10 +3,10 @@
 # Prevent committing badly formatted code
 
 cargo fmt -- --check
-if [ $? -ne 0 ]; then
-	echo "Run \`cargo fmt\` to fix formatting issues before committing."
-	exit 1
-fi
+#if [ $? -ne 0 ]; then
+#	echo "Run \`cargo fmt\` to fix formatting issues before committing."
+#	exit 1
+#fi
 
 dprint check
 if [ $? -ne 0 ]; then
