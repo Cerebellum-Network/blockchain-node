f235302e36e524fe8a2de9901361cdadd09059d3
Maksim Ramanenkau <cbnhrpo@gmail.com>
2024-03-05 21:14:00 +0100

    Try to implement set validators via extrinsic

--- pallets/validators/src/weights.rs
+++ pallets/validators/src/weights.rs
@@ -7,19 +7,19 @@ use sp_std::marker::PhantomData;
 
 /// Weight functions needed for pallet_erc20.
 pub trait WeightInfo {
-    fn add_validators() -> Weight;
+    fn set_validators() -> Weight;
 }
 
 pub struct SubstrateWeight<T>(PhantomData<T>);
 impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
-    fn add_validators() -> Weight {
+    fn set_validators() -> Weight {
         Weight::from_parts(195_000_000u64, 0)
     }
 }
 
 // For backwards compatibility and tests
 impl WeightInfo for () {
-    fn add_validators() -> Weight {
+    fn set_validators() -> Weight {
         Weight::from_parts(195_000_000u64, 0)
     }
 }
