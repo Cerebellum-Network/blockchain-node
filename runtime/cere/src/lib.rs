34e74219a0d1d15ec8104bb6d4faa4239845a2da
Maksim Ramanenkau <cbnhrpo@gmail.com>
2024-04-13 14:27:57 +0400

    Set Balances storage version (#304)
    ### Description
    <!-- Describe what change this PR is implementing -->
    
    ### Types of Changes
    <!--- What types of changes does your code introduce? -->
    - [ ] Tech Debt (Code improvements)
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Dependency upgrade (A change in substrate or any 3rd party crate
    version)
    
    ### Migrations and Hooks
    <!--- Check the following box with an x if the following applies: -->
    - [ ] This change requires a runtime migration.
    - [ ] Modifies `on_initialize`
    - [ ] Modifies `on_finalize`
    
    ### Checklist
    <!--- All boxes need to be checked. Follow this checklist before
    requiring PR review -->
    - [x] Change has been tested locally.
    - [ ] Change adds / updates tests.
    - [ ] Changelog doc updated.

--- runtime/cere/src/lib.rs
+++ runtime/cere/src/lib.rs
@@ -28,12 +28,12 @@ use frame_election_provider_support::{onchain, BalancingConfig, SequentialPhragm
 use frame_support::{
 	construct_runtime,
 	dispatch::DispatchClass,
-	pallet_prelude::Get,
+	pallet_prelude::{Get, StorageVersion},
 	parameter_types,
 	traits::{
 		ConstBool, ConstU128, ConstU16, ConstU32, Currency, EitherOfDiverse, EqualPrivilegeOnly,
-		Everything, Imbalance, InstanceFilter, KeyOwnerProofSystem, LockIdentifier, Nothing,
-		OnUnbalanced, WithdrawReasons,
+		Everything, GetStorageVersion, Imbalance, InstanceFilter, KeyOwnerProofSystem,
+		LockIdentifier, Nothing, OnRuntimeUpgrade, OnUnbalanced, WithdrawReasons,
 	},
 	weights::{
 		constants::{
@@ -1484,6 +1484,22 @@ impl Get<&'static str> for StakingMigrationV11OldPallet {
 	}
 }
 
+// We don't need to run pallet_balances::pallets::MigrateToTrackInactive or
+// pallet_balances::pallets::MigrateManyToTrackInactive since XCM related only.
+// MigrateToTrackInactive and MigrateManyToTrackInactive simply add CheckingAccount value to
+// InactiveIssuance so it's safe to skip it.
+pub struct SetBalancesStorageVersions;
+impl OnRuntimeUpgrade for SetBalancesStorageVersions {
+	fn on_runtime_upgrade() -> Weight {
+		let storage_version = <Balances>::on_chain_storage_version();
+		if storage_version < 1 {
+			StorageVersion::new(1).put::<Balances>();
+		}
+
+		RocksDbWeight::get().reads_writes(1, 1)
+	}
+}
+
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic =
 	generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>;
@@ -1492,15 +1508,7 @@ pub type SignedPayload = generic::SignedPayload<RuntimeCall, SignedExtra>;
 /// Extrinsic type that has already been checked.
 pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, RuntimeCall, SignedExtra>;
 /// Runtime migrations
-type Migrations = (
-	// Contracts migrate in sequence so make them last.
-	// Substrate upgrades run in reverse order so this migration
-	// is the last one to execute.
-	pallet_contracts::migration::Migration<Runtime>,
-	pallet_im_online::migration::v1::Migration<Runtime>,
-	pallet_democracy::migrations::v1::v1::Migration<Runtime>,
-	pallet_fast_unstake::migrations::v1::MigrateToV1<Runtime>,
-);
+type Migrations = SetBalancesStorageVersions;
 
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
