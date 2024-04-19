a33b9c53f229016425d9906e79fc9c04e09170b6
yahortsaryk <egortsaryk@gmail.com>
2024-04-19 12:54:21 +0200

    feat: inflation parameters changed
    wip

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -133,7 +133,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 51300,
+	spec_version: 51400,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 15,
@@ -507,8 +507,8 @@ impl pallet_session::historical::Config for Runtime {
 
 pallet_staking_reward_curve::build! {
 	const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
-		min_inflation: 0_000_200,
-		max_inflation: 0_100_000,
+		min_inflation: 0_000_100,
+		max_inflation: 0_050_000,
 		ideal_stake: 0_200_000,
 		falloff: 0_050_000,
 		max_piece_count: 100,
@@ -909,7 +909,7 @@ parameter_types! {
 	pub const ProposalBond: Permill = Permill::from_percent(5);
 	pub const ProposalBondMinimum: Balance = 50_000 * DOLLARS;
 	pub const SpendPeriod: BlockNumber = DAYS;
-	pub const Burn: Permill = Permill::from_parts(580);
+	pub const Burn: Permill = Permill::from_parts(25000);
 	pub const TipCountdown: BlockNumber = DAYS;
 	pub const TipFindersFee: Percent = Percent::from_percent(20);
 	pub const TipReportDepositBase: Balance = 50_000 * DOLLARS;
