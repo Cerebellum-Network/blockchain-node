f0269aef7c0670175c3563ae5a8bd1b3abcd70d4
Maksim Ramanenkau <cbnhrpo@gmail.com>
2024-05-02 15:35:50 +0200

    Fix clippy

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,9 +4,9 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-rustup install nightly-2024-03-12
+rustup install nightly-2023-10-05
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2024-03-12
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-10-05
 
 ln -sf $PWD/scripts/pre-commit.sh $PWD/.git/hooks/pre-commit || true
 ln -sf $PWD/scripts/pre-push.sh $PWD/.git/hooks/pre-push || true
