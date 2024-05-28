261beb835834f44653c5f831954996e0527b2327
Ayush Mishra <ayushmishra2005@gmail.com>
2024-05-28 16:51:33 +0530

    Extended verification pallet to have off chain worker capabilities (#343)
    ## Description
    <!-- Describe what change this PR is implementing -->
    
    ## Types of Changes
    Please select the branch type you are merging and fill in the relevant
    template.
    <!--- Check the following box with an x if the following applies: -->
    - [ ] Hotfix
    - [ ] Release
    - [ ] Fix or Feature
    
    ## Fix or Feature
    <!--- Check the following box with an x if the following applies: -->
    
    ### Types of Changes
    <!--- What types of changes does your code introduce? -->
    - [ ] Tech Debt (Code improvements)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Dependency upgrade (A change in substrate or any 3rd party crate
    version)
    
    ### Migrations and Hooks
    <!--- Check the following box with an x if the following applies: -->
    - [ ] This change requires a runtime migration.
    - [ ] Modifies `on_initialize`
    - [ ] Modifies `on_finalize`
    
    ### Checklist for Fix or Feature
    <!--- All boxes need to be checked. Follow this checklist before
    requiring PR review -->
    - [ ] Change has been tested locally.
    - [x] Change adds / updates tests if applicable.
    - [ ] Changelog doc updated.
    
    ## Checklist for Hotfix
    <!--- All boxes need to be checked. Follow this checklist before
    requiring PR review -->
    - [ ] Change has been deployed to Testnet.
    - [ ] Change has been tested in Testnet.
    - [ ] Changelog has been updated.
    - [ ] Crate version has been updated.
    - [ ] Spec version has been updated.
    - [ ] Transaction version has been updated if required.
    - [ ] Pull Request to `dev` has been created.
    - [ ] Pull Request to `staging` has been created.
    
    ## Checklist for Release
    <!--- All boxes need to be checked. Follow this checklist before
    requiring PR review -->
    - [ ] Change has been deployed to Devnet.
    - [ ] Change has been tested in Devnet.
    - [ ] Change has been deployed to Qanet.
    - [ ] Change has been tested in Qanet.
    - [ ] Change has been deployed to Testnet.
    - [ ] Change has been tested in Testnet.
    - [ ] Changelog has been updated.
    - [ ] Crate version has been updated.
    - [ ] Spec version has been updated.
    - [ ] Transaction version has been updated if required.

--- pallets/ddc-verification/src/tests.rs
+++ pallets/ddc-verification/src/tests.rs
@@ -1,15 +1,15 @@
 use ddc_primitives::ClusterId;
 use frame_support::{assert_noop, assert_ok};
-use sp_core::H256;
+use sp_core::{Pair, H256};
 
-use crate::{mock::*, Error, Event};
+use crate::{mock::*, Error, Event, *};
 
 #[test]
 fn create_billing_reports_works() {
 	new_test_ext().execute_with(|| {
 		System::set_block_number(1);
 
-		let dac_account = 2u64;
+		let dac_account = AccountId::from([1; 32]);
 		let cluster_id = ClusterId::from([12; 20]);
 		let era = 100;
 		let merkel_root_hash: H256 = array_bytes::hex_n_into_unchecked(
@@ -17,11 +17,10 @@ fn create_billing_reports_works() {
 		);
 
 		assert_ok!(DdcVerification::create_billing_reports(
-			RuntimeOrigin::signed(dac_account),
+			RuntimeOrigin::signed(dac_account.clone()),
 			cluster_id,
 			era,
 			merkel_root_hash,
-			vec![],
 		));
 
 		System::assert_last_event(Event::BillingReportCreated { cluster_id, era }.into());
@@ -35,9 +34,140 @@ fn create_billing_reports_works() {
 				cluster_id,
 				era,
 				merkel_root_hash,
-				vec![],
 			),
 			Error::<Test>::BillingReportAlreadyExist
 		);
+	})
+}
+
+#[test]
+fn set_validate_payout_batch_works() {
+	new_test_ext().execute_with(|| {
+		System::set_block_number(1);
+
+		let (pair1, _seed) = sp_core::sr25519::Pair::from_phrase(
+			"spider sell nice animal border success square soda stem charge caution echo",
+			None,
+		)
+		.unwrap();
+		let (pair2, _seed) = sp_core::sr25519::Pair::from_phrase(
+			"ketchup route purchase humble harsh true glide chef buyer crane infant sponsor",
+			None,
+		)
+		.unwrap();
+		let (pair3, _seed) = sp_core::sr25519::Pair::from_phrase(
+			"hamster diamond design extra december body action relax front sustain heavy gaze",
+			None,
+		)
+		.unwrap();
+		let (pair4, _seed) = sp_core::sr25519::Pair::from_phrase(
+			"clip olympic snack fringe critic claim chaos mother twist shy rule violin",
+			None,
+		)
+		.unwrap();
+		let (pair5, _seed) = sp_core::sr25519::Pair::from_phrase(
+			"bamboo fish such plug arrive vague umbrella today glass venture hour ginger",
+			None,
+		)
+		.unwrap();
+		let (pair6, _seed) = sp_core::sr25519::Pair::from_phrase(
+			"shallow radio below sudden unlock apology brisk shiver hill amateur tiny judge",
+			None,
+		)
+		.unwrap();
+
+		let validator_one: <Test as Config>::AuthorityId = pair1.public().into();
+		let validator_two: <Test as Config>::AuthorityId = pair2.public().into();
+		let validator_three: <Test as Config>::AuthorityId = pair3.public().into();
+		let validator_four: <Test as Config>::AuthorityId = pair4.public().into();
+		let validator_six: <Test as Config>::AuthorityId = pair6.public().into();
+
+		ValidatorSet::<Test>::put(vec![
+			validator_one,
+			validator_two,
+			validator_three,
+			validator_four,
+			validator_six,
+		]);
+
+		let account_id1 = AccountId::from(pair1.public().0);
+		let account_id2 = AccountId::from(pair2.public().0);
+		let account_id3 = AccountId::from(pair3.public().0);
+		let account_id4 = AccountId::from(pair4.public().0);
+		let account_id6 = AccountId::from(pair6.public().0);
+		let cluster_id = ClusterId::from([12; 20]);
+		let era = 100;
+		let payout_data = PayoutData { hash: MmrRootHash::default() };
+		let payout_data1 = PayoutData { hash: MmrRootHash::from_low_u64_ne(1) };
+
+		// 1. If user is not part of validator, he/she won't be able to sign extrinsic
+		assert_noop!(
+			DdcVerification::set_validate_payout_batch(
+				RuntimeOrigin::signed(AccountId::from(pair5.public().0)),
+				cluster_id,
+				era,
+				payout_data.clone(),
+			),
+			Error::<Test>::NotAValidator
+		);
+
+		// 2. send signed transaction from valid validator
+		assert_ok!(DdcVerification::set_validate_payout_batch(
+			RuntimeOrigin::signed(account_id1.clone()),
+			cluster_id,
+			era,
+			payout_data.clone(),
+		));
+
+		// 3. If validator already sent the data, he/she won't be able to submit same transaction
+		assert_noop!(
+			DdcVerification::set_validate_payout_batch(
+				RuntimeOrigin::signed(AccountId::from(account_id1)),
+				cluster_id,
+				era,
+				payout_data.clone(),
+			),
+			Error::<Test>::AlreadySigned
+		);
+
+		// 4. send signed transaction from second valid validator
+		assert_ok!(DdcVerification::set_validate_payout_batch(
+			RuntimeOrigin::signed(account_id2.clone()),
+			cluster_id,
+			era,
+			payout_data.clone(),
+		));
+
+		// 5. 2/3 rd validators have not signed yet the same data
+		assert_eq!(DdcVerification::payout_batch(cluster_id, era), None);
+
+		assert_ok!(DdcVerification::set_validate_payout_batch(
+			RuntimeOrigin::signed(account_id3.clone()),
+			cluster_id,
+			era,
+			payout_data.clone(),
+		));
+
+		// 6. send signed transaction from third valid validator but different hash
+		assert_ok!(DdcVerification::set_validate_payout_batch(
+			RuntimeOrigin::signed(account_id6.clone()),
+			cluster_id,
+			era,
+			payout_data1.clone(),
+		));
+
+		// 7. 2/3 rd validators have not signed yet the same data
+		assert_eq!(DdcVerification::payout_batch(cluster_id, era), None);
+
+		// 8. send signed transaction from fourth valid validator
+		assert_ok!(DdcVerification::set_validate_payout_batch(
+			RuntimeOrigin::signed(account_id4.clone()),
+			cluster_id,
+			era,
+			payout_data.clone(),
+		));
+
+		// 9. 2/3rd validators have sent the same data with same hash
+		assert_eq!(DdcVerification::payout_batch(cluster_id, era).unwrap(), payout_data);
 	})
 }
