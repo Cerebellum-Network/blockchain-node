706e354adf4855aed6f84ea210af79b38dee3545
Vic Genin <victor.genin@live.com>
2024-07-20 08:25:28 +0300

    migrations

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -1379,6 +1379,7 @@ pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, RuntimeCall, Si
 type Migrations = (
 	pallet_ddc_clusters::migrations::v2::MigrateToV2<Runtime>,
 	pallet_ddc_staking::migrations::v1::MigrateToV1<Runtime>,
+	pallet_ddc_customers::migration::MigrateToV2<Runtime>,
 	pallet_ddc_customers::migration::MigrateToV1<Runtime>,
 );
 
