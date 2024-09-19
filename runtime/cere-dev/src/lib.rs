8365a2b18fd0a43d23d092fd3325401f1258134a
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2024-09-19 13:16:25 +0530

    refactoring

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -1307,7 +1307,7 @@ impl pallet_ddc_verification::Config for Runtime {
 	type OffchainIdentifierId = ddc_primitives::crypto::OffchainIdentifierId;
 	type ActivityHasher = BlakeTwo256;
 	const MAJORITY: u8 = 67;
-	const BLOCK_TO_START: u16 = 100; // every 100 blocks
+	const BLOCK_TO_START: u16 = 30; // every 100 blocks
 	const MIN_DAC_NODES_FOR_CONSENSUS: u16 = 3;
 	const MAX_PAYOUT_BATCH_SIZE: u16 = MAX_PAYOUT_BATCH_SIZE;
 	const MAX_PAYOUT_BATCH_COUNT: u16 = MAX_PAYOUT_BATCH_COUNT;
