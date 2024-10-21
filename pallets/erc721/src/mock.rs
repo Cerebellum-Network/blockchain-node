0deafe2aa818122dc4a2a9d6b4e9942d27550c27
Aideepakchaudhary <aideepakchaudhary@gmail.com>
2024-10-21 11:46:39 +0530

    fix frame system macro

--- pallets/erc721/src/mock.rs
+++ pallets/erc721/src/mock.rs
@@ -22,7 +22,7 @@ parameter_types! {
 	pub const AvailableBlockRatio: Perbill = Perbill::one();
 }
 
-#[derive_impl(frame_system::config_preludes::ParaChainDefaultConfig as frame_system::DefaultConfig)]
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
 impl frame_system::Config for Test {
 	type BaseCallFilter = Everything;
 	type RuntimeOrigin = RuntimeOrigin;
