a5ea0bb49a1161aa262b36bc09ac3b8c58fa9858
Aideepakchaudhary <aideepakchaudhary@gmail.com>
2024-10-23 13:09:11 +0530

    remove getter from ddc-clusters-gov

--- pallets/ddc-clusters-gov/src/lib.rs
+++ pallets/ddc-clusters-gov/src/lib.rs
@@ -150,7 +150,6 @@ pub mod pallet {
 	}
 
 	#[pallet::storage]
-	#[pallet::getter(fn proposal_of)]
 	pub type ClusterProposal<T: Config> = StorageMap<
 		_,
 		Identity,
@@ -161,13 +160,11 @@ pub mod pallet {
 
 	/// Votes on a given cluster proposal, if it is ongoing.
 	#[pallet::storage]
-	#[pallet::getter(fn voting)]
 	pub type ClusterProposalVoting<T: Config> =
 		StorageMap<_, Identity, ClusterId, Votes<T::AccountId, BlockNumberFor<T>>, OptionQuery>;
 
 	/// Public referendums initiated by clusters
 	#[pallet::storage]
-	#[pallet::getter(fn submission_depositor)]
 	pub type SubmissionDeposits<T: Config> =
 		StorageMap<_, Identity, ReferendumIndex, SubmissionDeposit<T::AccountId>, OptionQuery>;
 
@@ -534,7 +531,7 @@ pub mod pallet {
 			cluster_id: ClusterId,
 			approve: bool,
 		) -> Result<bool, DispatchError> {
-			let mut voting = Self::voting(cluster_id).ok_or(Error::<T>::ProposalMissing)?;
+			let mut voting = ClusterProposalVoting::<T>::get(cluster_id).ok_or(Error::<T>::ProposalMissing)?;
 
 			let position_yes = voting.ayes.iter().position(|a| a == &voter_id);
 			let position_no = voting.nays.iter().position(|a| a == &voter_id);
@@ -579,7 +576,7 @@ pub mod pallet {
 
 		/// Close a vote that is either approved, disapproved or whose voting period has ended.
 		fn do_close(cluster_id: ClusterId, caller_id: T::AccountId) -> DispatchResultWithPostInfo {
-			let voting = Self::voting(cluster_id).ok_or(Error::<T>::ProposalMissing)?;
+			let voting = ClusterProposalVoting::<T>::get(cluster_id).ok_or(Error::<T>::ProposalMissing)?;
 
 			let mut no_votes = voting.nays.len() as MemberCount;
 			let mut yes_votes = voting.ayes.len() as MemberCount;
