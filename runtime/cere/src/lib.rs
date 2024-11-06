5fd3d1c4a6617bcd0d61f25426a90b08e3a47030
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2024-11-06 21:29:16 +0530

    refactoring

--- runtime/cere/src/lib.rs
+++ runtime/cere/src/lib.rs
@@ -1452,7 +1452,7 @@ type Migrations = (
 	pallet_nomination_pools::migration::versioned_migrations::V6ToV7<Runtime>,
 	pallet_staking::migrations::v14::MigrateToV14<Runtime>,
 	pallet_grandpa::migrations::MigrateV4ToV5<Runtime>,
-	migrations::Unreleased,
+	// migrations::Unreleased,
 );
 
 pub mod migrations {
