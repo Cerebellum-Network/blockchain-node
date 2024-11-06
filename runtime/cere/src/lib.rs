f97c8bb6d8302a74f301cd2459d979c0190d53af
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2024-11-06 17:25:44 +0530

    Fixed spec version and pull zombienet fix from dev

--- runtime/cere/src/lib.rs
+++ runtime/cere/src/lib.rs
@@ -1452,6 +1452,7 @@ type Migrations = (
 	pallet_nomination_pools::migration::versioned_migrations::V6ToV7<Runtime>,
 	pallet_staking::migrations::v14::MigrateToV14<Runtime>,
 	pallet_grandpa::migrations::MigrateV4ToV5<Runtime>,
+	pallet_ddc_verification::migrations::v1::MigrateToV1<Runtime>,
 	// This should be carefully enabled in the server, if not enabled
 	// migrations::Unreleased,
 );
