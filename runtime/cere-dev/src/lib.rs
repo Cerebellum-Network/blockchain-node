da97857899006d9907f88cf507feabf08aab30df
yahortsaryk <egortsaryk@gmail.com>
2024-11-07 16:34:27 +0100

    fix: 'transaction_version' is fixed

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -156,7 +156,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 61003,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 24,
+	transaction_version: 22,
 	state_version: 0,
 };
 
