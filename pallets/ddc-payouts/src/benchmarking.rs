4c37f79505b730437d9c6985795e5fe1d8d344f4
yahortsaryk <egortsaryk@gmail.com>
2024-11-07 03:58:02 +0100

    fix: unussed variables are removed and clippy issues fixed

--- pallets/ddc-payouts/src/benchmarking.rs
+++ pallets/ddc-payouts/src/benchmarking.rs
@@ -225,7 +225,7 @@ benchmarks! {
 		});
 
 		let batch_index: BatchIndex = 0;
-		let payers: Vec<(T::AccountId, String, BucketId, CustomerUsage)> = (0..b).map(|i| {
+		let payers: Vec<(NodePubKey, BucketId, CustomerUsage)> = (0..b).map(|i| {
 			let customer = create_account::<T>("customer", i, i);
 
 			if b % 2 == 0 {
@@ -243,9 +243,12 @@ benchmarks! {
 				number_of_puts: 5, // 5 puts
 			};
 			let bucket_id: BucketId = 1;
-			let node_id: String = String::from("0x302f937df3a0ec4c658e8122439e748d227442ebd493cef521a1e14943844395");
+			let node_key = NodePubKey::StoragePubKey(AccountId32::from([
+				48, 47, 147, 125, 243, 160, 236, 76, 101, 142, 129, 34, 67, 158, 116, 141, 34, 116, 66,
+				235, 212, 147, 206, 245, 33, 161, 225, 73, 67, 132, 67, 149,
+			]));
 
-			(customer, node_id, bucket_id, customer_usage)
+			(node_key, bucket_id, customer_usage)
 		}).collect();
 
 	}: _(RawOrigin::Signed(dac_account.clone()), cluster_id, era, batch_index, payers, MMRProof::default())
