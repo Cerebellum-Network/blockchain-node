74f63bcfde9d2f80f4bfb355dc864e44610f6d67
Alisher A. Khassanov <a.khssnv@gmail.com>
2024-12-10 15:19:04 +0500

    Bump `spec_version`

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -153,7 +153,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 63004,
+	spec_version: 63005,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 24,
