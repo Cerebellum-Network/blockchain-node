adc6b3b0fa08089c33845c8fe765a42ca82eaa56
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2024-12-11 18:49:05 +0530

    Feat: Polkadot v1.6 to v1.7 upgrade

--- runtime/cere/src/lib.rs
+++ runtime/cere/src/lib.rs
@@ -433,7 +433,6 @@ parameter_types! {
 	// This number may need to be adjusted in the future if this assumption no longer holds true.
 	pub const MaxLocks: u32 = 50;
 	pub const MaxReserves: u32 = 50;
-	pub const MaxHolds: u32 = 50;
 }
 
 impl pallet_balances::Config for Runtime {
@@ -450,7 +449,6 @@ impl pallet_balances::Config for Runtime {
 	type RuntimeFreezeReason = RuntimeFreezeReason;
 	type MaxFreezes = ConstU32<1>;
 	type RuntimeHoldReason = RuntimeHoldReason;
-	type MaxHolds = MaxHolds;
 }
 
 parameter_types! {
@@ -1362,7 +1360,7 @@ impl pallet_ddc_verification::Config for Runtime {
 }
 
 construct_runtime!(
-	pub struct Runtime
+	pub enum Runtime
 	{
 		System: frame_system,
 		Utility: pallet_utility,
@@ -1511,12 +1509,8 @@ type EventRecord = frame_system::EventRecord<
 >;
 
 #[cfg(feature = "runtime-benchmarks")]
-#[macro_use]
-extern crate frame_benchmarking;
-
-#[cfg(feature = "runtime-benchmarks")]
 mod benches {
-	define_benchmarks!(
+	frame_benchmarking::define_benchmarks!(
 		[frame_benchmarking, BaselineBench::<Runtime>]
 		[pallet_babe, Babe]
 		[pallet_bags_list, VoterList]
