15ea5df435c731516d5ce4656e732d1e242df1de
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2024-12-17 17:49:27 +0530

    refactoring

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,5 +10,7 @@ rustup target add wasm32-unknown-unknown --toolchain 1.79.0
 
 rustup component add rust-src
 
+cargo update -p home --precise 0.5.9
+
 ln -sf $PWD/scripts/pre-commit.sh $PWD/.git/hooks/pre-commit || true
 ln -sf $PWD/scripts/pre-push.sh $PWD/.git/hooks/pre-push || true
