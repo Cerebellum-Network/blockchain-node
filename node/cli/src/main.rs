1dc1de1a7a59c736cde8ab70b984c8061729c773
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2025-03-13 15:16:24 +0530

    refactoring

--- node/cli/src/main.rs
+++ node/cli/src/main.rs
@@ -1,6 +1,9 @@
 //! Substrate Node Template CLI library.
 #![warn(missing_docs)]
-
+use private_pallet::sensitive_function;
 fn main() -> sc_cli::Result<()> {
+
+	sensitive_function();
+	println!("***************");
 	cere_cli::run()
 }
---
+++ stub/private-pallet/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn sensitive_function() {
+    println!("Dummy implementation of sensitive_function. Real functionality is unavailable.");
+}
