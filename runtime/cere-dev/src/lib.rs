1794b09cb795cf308a9e82fe8c481104fe352b12
Ayush Kumar Mishra <ayushmishra2005@gmail.com>
2025-04-07 15:17:49 +0200

    refactoring

--- runtime/cere-dev/src/lib.rs
+++ runtime/cere-dev/src/lib.rs
@@ -168,7 +168,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 73009,
+	spec_version: 73010,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 24,
