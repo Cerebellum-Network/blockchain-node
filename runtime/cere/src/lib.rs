b37116057187bfc99600931667cda49e8f65d0df
jaxter03 <krishna.singh9926@gmail.com>
2025-04-15 13:45:01 +0530

    Updated spec version

--- runtime/cere/src/lib.rs
+++ runtime/cere/src/lib.rs
@@ -159,7 +159,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 731089,
+	spec_version: 73109,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 24,
