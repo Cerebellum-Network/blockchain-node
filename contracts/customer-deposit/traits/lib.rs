d249d3477bf60330223fe678c5c7be187385085a
yahortsaryk <egortsaryk@gmail.com>
2025-07-08 23:56:21 +0200

    fix: formatting issues

--- contracts/customer-deposit/traits/lib.rs
+++ contracts/customer-deposit/traits/lib.rs
@@ -1,16 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-use ink::env::Environment;
-use ink::prelude::vec::Vec;
+use ink::{env::Environment, prelude::vec::Vec};
 
 type AccountId = <ink::env::DefaultEnvironment as Environment>::AccountId;
 
 #[ink::trait_definition]
 pub trait DdcPayouts {
-    #[ink(message)]
-    fn charge(
-        &mut self,
-        vault: AccountId,
-        batch: Vec<(AccountId, u128)>,
-    ) -> Result<Vec<(AccountId, u128)>, ()>;
+	#[ink(message)]
+	fn charge(
+		&mut self,
+		vault: AccountId,
+		batch: Vec<(AccountId, u128)>,
+	) -> Result<Vec<(AccountId, u128)>, ()>;
 }
