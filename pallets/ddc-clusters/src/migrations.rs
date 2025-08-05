f4368a5ce30526fb52378309ea1dd0d4d87fbe85
yahortsaryk <egortsaryk@gmail.com>
2025-08-05 14:17:16 +0200

    chore: migration for cluster protocol params

--- pallets/ddc-clusters/src/migrations.rs
+++ pallets/ddc-clusters/src/migrations.rs
@@ -1,7 +1,7 @@
 use frame_support::{storage_alias, traits::OnRuntimeUpgrade};
 use log::info;
 use serde::{Deserialize, Serialize};
-use sp_runtime::Saturating;
+use sp_runtime::{Saturating, Perquintill};
 
 use super::*;
 
@@ -484,6 +484,31 @@ pub mod v3 {
 		Cluster<<T as frame_system::Config>::AccountId>,
 	>;
 
+	#[derive(
+		Clone, Encode, Decode, DecodeWithMemTracking, RuntimeDebug, TypeInfo, PartialEq, Default, Serialize, Deserialize,
+	)]
+	#[scale_info(skip_type_params(Balance, BlockNumber, T))]
+	pub struct ClusterProtocolParams<Balance, BlockNumber> {
+		pub treasury_share: Perquintill,
+		pub validators_share: Perquintill,
+		pub cluster_reserve_share: Perquintill,
+		pub storage_bond_size: Balance,
+		pub storage_chill_delay: BlockNumber,
+		pub storage_unbonding_delay: BlockNumber,
+		pub unit_per_mb_stored: u128,
+		pub unit_per_mb_streamed: u128,
+		pub unit_per_put_request: u128,
+		pub unit_per_get_request: u128,
+	}
+
+	#[storage_alias]
+	pub type ClustersGovParams<T: Config> = StorageMap<
+		crate::Pallet<T>,
+		Twox64Concat,
+		ClusterId,
+		ClusterProtocolParams<BalanceOf<T>, BlockNumberFor<T>>,
+	>;
+
 	use super::*;
 	pub fn migrate_to_v3<T: Config>() -> Weight {
 		let on_chain_version = Pallet::<T>::on_chain_storage_version();
@@ -578,92 +603,139 @@ pub mod v3 {
 			Ok(())
 		}
 	}
+}
 
-	//TODO: Fix the test in another PR
-	// #[cfg(test)]
-	// #[cfg(feature = "try-runtime")]
-	// mod test {
-	// 	use ddc_primitives::ClusterStatus;
-	// 	use frame_support::pallet_prelude::StorageVersion;
-	//
-	// 	use super::*;
-	// 	use crate::mock::{Test as T, *};
-	//
-	// 	#[test]
-	// 	fn cluster_migration_works() {
-	// 		ExtBuilder.build_and_execute(|| {
-	// 			let cluster_id0 = ClusterId::from([0; 20]);
-	// 			let cluster_id1 = ClusterId::from([1; 20]);
-	// 			let cluster_id2 = ClusterId::from([2; 20]);
-	// 			let cluster_manager_id = AccountId::from([1; 32]);
-	// 			let cluster_reserve_id = AccountId::from([2; 32]);
-	// 			let auth_contract = AccountId::from([3; 32]);
-	//
-	// 			assert_eq!(StorageVersion::get::<Pallet<T>>(), 2);
-	//
-	// 			let cluster1 = Cluster {
-	// 				cluster_id: cluster_id1,
-	// 				manager_id: cluster_manager_id.clone(),
-	// 				reserve_id: cluster_reserve_id.clone(),
-	// 				props: ClusterProps {
-	// 					node_provider_auth_contract: Some(auth_contract.clone()),
-	// 					erasure_coding_required: 16,
-	// 					erasure_coding_total: 48,
-	// 					replication_total: 20,
-	// 				},
-	// 				last_paid_era: 0,
-	// 				status: ClusterStatus::Activated,
-	// 			};
-	//
-	// 			Clusters::<T>::insert(cluster_id1, cluster1);
-	// 			let cluster2 = Cluster {
-	// 				cluster_id: cluster_id2,
-	// 				manager_id: cluster_manager_id,
-	// 				reserve_id: cluster_reserve_id,
-	// 				props: ClusterProps {
-	// 					node_provider_auth_contract: Some(auth_contract.clone()),
-	// 					erasure_coding_required: 16,
-	// 					erasure_coding_total: 48,
-	// 					replication_total: 20,
-	// 				},
-	// 				last_paid_era: 0,
-	// 				status: ClusterStatus::Activated,
-	// 			};
-	//
-	// 			Clusters::<T>::insert(cluster_id2, cluster2);
-	// 			let cluster_count = Clusters::<T>::iter_values().count() as u32;
-	//
-	// 			assert_eq!(cluster_count, 3);
-	// 			let state = MigrateToV3::<T>::pre_upgrade().unwrap();
-	// 			let _weight = MigrateToV3::<T>::on_runtime_upgrade();
-	// 			MigrateToV3::<T>::post_upgrade(state).unwrap();
-	//
-	// 			let cluster_count_after_upgrade = Clusters::<T>::iter_values().count() as u32;
-	//
-	// 			assert_eq!(StorageVersion::get::<Pallet<T>>(), 3);
-	// 			assert_eq!(cluster_count_after_upgrade, 3);
-	// 			assert_eq!(
-	// 				Clusters::<T>::get(cluster_id0).unwrap().props.erasure_coding_required,
-	// 				16
-	// 			);
-	// 			assert_eq!(Clusters::<T>::get(cluster_id0).unwrap().props.erasure_coding_total, 48);
-	// 			assert_eq!(Clusters::<T>::get(cluster_id0).unwrap().props.replication_total, 20);
-	// 			assert_eq!(Clusters::<T>::get(cluster_id0).unwrap().last_paid_era, 0);
-	// 			assert_eq!(
-	// 				Clusters::<T>::get(cluster_id1).unwrap().props.erasure_coding_required,
-	// 				16
-	// 			);
-	// 			assert_eq!(Clusters::<T>::get(cluster_id1).unwrap().props.erasure_coding_total, 48);
-	// 			assert_eq!(Clusters::<T>::get(cluster_id1).unwrap().props.replication_total, 20);
-	// 			assert_eq!(Clusters::<T>::get(cluster_id1).unwrap().last_paid_era, 0);
-	// 			assert_eq!(
-	// 				Clusters::<T>::get(cluster_id2).unwrap().props.erasure_coding_required,
-	// 				16
-	// 			);
-	// 			assert_eq!(Clusters::<T>::get(cluster_id2).unwrap().props.erasure_coding_total, 48);
-	// 			assert_eq!(Clusters::<T>::get(cluster_id2).unwrap().props.replication_total, 20);
-	// 			assert_eq!(Clusters::<T>::get(cluster_id2).unwrap().last_paid_era, 0);
-	// 		});
-	// 	}
-	// }
+pub mod v4 {
+	use super::*;
+	use crate::BalanceOf;
+	use frame_support::{
+		pallet_prelude::*,
+		traits::{Get},
+		weights::Weight,
+	};
+
+	#[derive(
+		Clone, Encode, Decode, DecodeWithMemTracking, RuntimeDebug, TypeInfo, PartialEq, Default, Serialize, Deserialize,
+	)]
+	#[scale_info(skip_type_params(Balance, BlockNumber, T))]
+	pub struct ClusterProtocolParams<Balance, BlockNumber, AccountId> {
+		pub treasury_share: Perquintill,
+		pub validators_share: Perquintill,
+		pub cluster_reserve_share: Perquintill,
+		pub storage_bond_size: Balance,
+		pub storage_chill_delay: BlockNumber,
+		pub storage_unbonding_delay: BlockNumber,
+		pub unit_per_mb_stored: u128,
+		pub unit_per_mb_streamed: u128,
+		pub unit_per_put_request: u128,
+		pub unit_per_get_request: u128,
+		pub customer_deposit_contract: AccountId,
+	}
+
+	#[storage_alias]
+	pub type ClustersGovParams<T: Config> = StorageMap<
+		crate::Pallet<T>,
+		Twox64Concat,
+		ClusterId,
+		ClusterProtocolParams<BalanceOf<T>, BlockNumberFor<T>, <T as frame_system::Config>::AccountId>,
+	>;
+
+	pub fn migrate_to_v4<T: Config>() -> Weight {
+		let on_chain_version = Pallet::<T>::on_chain_storage_version();
+		let current_version = Pallet::<T>::in_code_storage_version();
+
+		info!(
+			target: LOG_TARGET,
+			"Running migration with current storage version {:?} / onchain {:?}",
+			current_version,
+			on_chain_version
+		);
+
+		if on_chain_version == 3 && current_version == 4 {
+			let mut translated = 0u64;
+			let count = v3::ClustersGovParams::<T>::iter().count();
+			info!(
+				target: LOG_TARGET,
+				" >>> Updating DDC Cluster protocol params storage. Migrating {} cluster protocol params...", count
+			);
+
+			v4::ClustersGovParams::<T>::translate::<v3::ClusterProtocolParams<BalanceOf<T>, BlockNumberFor<T>>, _>(
+				|cluster_id: ClusterId, old_cluster_params: v3::ClusterProtocolParams<BalanceOf<T>, BlockNumberFor<T>>| {
+					info!(target: LOG_TARGET, "     Migrating protocol params for cluster {:?} ...", cluster_id);
+					translated.saturating_inc();
+					
+					let bytes = [0u8; 32];
+					let default_customer_deposit_contract = T::AccountId::decode(&mut &bytes.encode()[..])
+						.expect("Customer deposit contract to be parsed");
+
+					Some(v4::ClusterProtocolParams {
+						treasury_share: old_cluster_params.treasury_share,
+						validators_share: old_cluster_params.validators_share,
+						cluster_reserve_share: old_cluster_params.cluster_reserve_share,
+						storage_bond_size: old_cluster_params.storage_bond_size,
+						storage_chill_delay: old_cluster_params.storage_chill_delay,
+						storage_unbonding_delay: old_cluster_params.storage_unbonding_delay,
+						unit_per_mb_stored: old_cluster_params.unit_per_mb_stored,
+						unit_per_mb_streamed: old_cluster_params.unit_per_mb_streamed,
+						unit_per_put_request: old_cluster_params.unit_per_put_request,
+						unit_per_get_request: old_cluster_params.unit_per_get_request,
+						customer_deposit_contract: default_customer_deposit_contract,
+					})
+				},
+			);
+
+			// Update storage version.
+			StorageVersion::new(4).put::<Pallet<T>>();
+			info!(
+				target: LOG_TARGET,
+				"Upgraded {} records, storage to version {:?}",
+				count,
+				current_version
+			);
+
+			T::DbWeight::get().reads_writes(translated + 1, translated + 1)
+		} else {
+			info!(target: LOG_TARGET, " >>> Unused migration!");
+			T::DbWeight::get().reads(1)
+		}
+	}
+
+	pub struct MigrateToV4<T>(PhantomData<T>);
+
+	impl<T: Config> OnRuntimeUpgrade for MigrateToV4<T> {
+		fn on_runtime_upgrade() -> Weight {
+			migrate_to_v4::<T>()
+		}
+
+		#[cfg(feature = "try-runtime")]
+		fn pre_upgrade() -> Result<Vec<u8>, sp_runtime::DispatchError> {
+			let prev_count = v3::ClustersGovParams::<T>::iter().count();
+
+			Ok((prev_count as u64).encode())
+		}
+
+		#[cfg(feature = "try-runtime")]
+		fn post_upgrade(prev_state: Vec<u8>) -> Result<(), sp_runtime::DispatchError> {
+			let prev_count: u64 = Decode::decode(&mut &prev_state[..])
+				.expect("pre_upgrade provides a valid state; qed");
+
+			let post_count = v4::ClustersGovParams::<T>::iter().count() as u64;
+			ensure!(
+				prev_count == post_count,
+				"the cluster protocol params count before and after the migration should be the same"
+			);
+
+			let current_version = Pallet::<T>::in_code_storage_version();
+			let on_chain_version = Pallet::<T>::on_chain_storage_version();
+
+			ensure!(current_version == 4, "must_upgrade");
+			ensure!(
+				current_version == on_chain_version,
+				"after migration, the current_version and on_chain_version should be the same"
+			);
+			Ok(())
+		}
+	}
+
+
 }
